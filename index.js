7dd7dd021d620051fc9e70e947420550edb64438
George Goodall <ggoodall@scottlogic.com>
2023-10-24 11:15:30 +0100

    now render helloWorld from nunjucks template

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const app = express();
 
+nunjucks.configure('src/views', {
+    express:  app
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.render('helloWorld.html');
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+    console.log('Server listening on port 3000');
 });
