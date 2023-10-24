89567ff5afe1f0b17b6f9d69fcc266c9ce01faf3
George Goodall <ggoodall@scottlogic.com>
2023-10-24 15:51:02 +0100

    added scss

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const app = express()
 
-nunjucks.configure('src/views', {
+const path = require('path')
+app.use('/public', express.static(path.join(__dirname, '/public')))
+app.use('/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk')))
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets')))
+
+nunjucks.configure([
+  'src/views',
+  'node_modules/govuk-frontend/'
+], {
   express: app
 })
 
-app.get('/', (req, res) => {
+app.get('/helloWorld', (req, res) => {
   const data = {
     title: 'Hello World',
     content: 'This is a sample content'
   }
   res.render('helloWorld.html', data)
+})
+
+app.get('/', (req, res) => {
+  const data = {}
+  res.render('index.html', data)
 })
 
 app.listen(3000, () => {
