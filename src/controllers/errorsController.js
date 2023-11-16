924ec8728a502feff6b877714c2cb506cad3c787
George Goodall <ggoodall@scottlogic.com>
2023-11-16 10:05:49 +0000

    refactor code

--- src/controllers/errorsController.js
+++ src/controllers/errorsController.js
@@ -3,20 +3,33 @@
 const MyController = require('./MyController')
 
 class ErrorsController extends MyController {
-  // ToDo: Not happy with this implementation, it's too complex, needs refactoring. can look at this after the demo
   get (req, res, next) {
-    const json = req.sessionModel.get('validationResult')
+    const validationResult = req.sessionModel.get('validationResult')
 
+    const { aggregatedIssues, issueCounts } = this.aggregateIssues(validationResult)
+
+    const rows = Object.values(aggregatedIssues)
+
+    req.form.options.rows = rows
+    req.form.options.issueCounts = issueCounts
+    req.form.options.columnNames = Object.keys(rows[0]) // ToDo: should the api return the columns here?
+    req.form.options.dataset = req.sessionModel.get('dataset')
+    req.form.options.dataSubject = req.sessionModel.get('data-subject')
+
+    super.get(req, res, next)
+  }
+
+  aggregateIssues (apiResponseData) {
     const aggregatedIssues = {}
     const issueCounts = {}
 
-    json['issue-log'].forEach(issue => {
+    apiResponseData['issue-log'].forEach(issue => {
       const entryNumber = issue['entry-number']
 
-      const rowValues = json['converted-csv'][issue['line-number'] - 2]
+      const rowValues = apiResponseData['converted-csv'][issue['line-number'] - 2]
       if (!(entryNumber in aggregatedIssues)) {
         aggregatedIssues[entryNumber] = Object.keys(rowValues).reduce((acc, originalColumnName) => {
-          const mappedColumnName = this.lookupMappedColumnNameFromOriginal(originalColumnName, json['column-field-log'])
+          const mappedColumnName = this.lookupMappedColumnNameFromOriginal(originalColumnName, apiResponseData['column-field-log'])
           acc[mappedColumnName] = {
             error: false,
             value: rowValues[originalColumnName]
@@ -28,26 +41,13 @@ class ErrorsController extends MyController {
       if (entryNumber in aggregatedIssues) {
         aggregatedIssues[entryNumber][issue.field] = {
           error: this.lookupIssueType(issue['issue-type']),
-          value: rowValues[this.lookupOriginalColumnNameFromMapped(issue.field, json['column-field-log'])]
+          value: rowValues[this.lookupOriginalColumnNameFromMapped(issue.field, apiResponseData['column-field-log'])]
         }
         issueCounts[issue.field] = issueCounts[issue.field] ? issueCounts[issue.field] + 1 : 1
       }
     })
 
-    const rows = Object.keys(aggregatedIssues).map(entryNumber => {
-      return {
-        entryNumber,
-        columns: aggregatedIssues[entryNumber]
-      }
-    })
-
-    req.form.options.rows = rows
-    req.form.options.issueCounts = issueCounts
-    req.form.options.columnNames = Object.keys(rows[0].columns) // ToDo: should the api return the columns here?
-    req.form.options.dataset = req.sessionModel.get('dataset')
-    req.form.options.dataSubject = req.sessionModel.get('data-subject')
-
-    super.get(req, res, next)
+    return { aggregatedIssues, issueCounts }
   }
 
   lookupIssueType (issueType) {
