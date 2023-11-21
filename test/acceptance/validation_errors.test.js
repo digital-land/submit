9435868be982e66feeabd97815c5b52fb7a04265
George Goodall <ggoodall@scottlogic.com>
2023-11-21 12:48:10 +0000

    added some validation error message testing

--- test/acceptance/validation_errors.test.js
+++ test/acceptance/validation_errors.test.js
@@ -7,18 +7,7 @@ test('when the user clicks continue on the data subject page without entering a 
 
   await page.waitForSelector('input#data-subject.govuk-radios__input')
 
-  const errorLink = await page.getByRole('link', { name: 'Please select a data subject' })
-  const fieldError = await page.getByText('Error: Please select a data subject')
-  const errorSummary = await page.getByText('There is a problem')
-
-  expect(await errorSummary.isVisible(), 'Page should show the error summary').toBeTruthy()
-  expect(await errorLink.isVisible(), 'Page should the error message that is a link to the problem field').toBeTruthy()
-  expect(await fieldError.isVisible(), 'Page should show the error message next to the problem field').toBeTruthy()
-  await errorLink.click()
-  const problemFieldIsFocused = await page.$eval('input#data-subject.govuk-radios__input', (el) => el === document.activeElement)
-  expect(problemFieldIsFocused, 'The focus should be on the problem field').toBeTruthy()
-
-  expect(await page.title(), 'Page title should indicate there\'s an error').toMatch(/Error: .*/)
+  await testErrorMessage(page, 'input#data-subject.govuk-radios__input', 'Please select a data subject')
 })
 
 test('when the user clicks continue on the dataset page without entering a dataset, the page correctly indicates there\'s an error', async ({ page }) => {
@@ -35,18 +24,7 @@ test('when the user clicks continue on the dataset page without entering a datas
 
   await page.waitForSelector('input#dataset.govuk-radios__input')
 
-  const errorLink = await page.getByRole('link', { name: 'Please select a dataset' })
-  const fieldError = await page.getByText('Error: Please select a dataset')
-  const errorSummary = await page.getByText('There is a problem')
-
-  expect(await errorSummary.isVisible(), 'Page should show the error summary').toBeTruthy()
-  expect(await errorLink.isVisible(), 'Page should the error message that is a link to the problem field').toBeTruthy()
-  expect(await fieldError.isVisible(), 'Page should show the error message next to the problem field').toBeTruthy()
-  await errorLink.click()
-  const problemFieldIsFocused = await page.$eval('input#dataset.govuk-radios__input', (el) => el === document.activeElement)
-  expect(problemFieldIsFocused, 'The focus should be on the problem field').toBeTruthy()
-
-  expect(await page.title(), 'Page title should indicate there\'s an error').toMatch(/Error: .*/)
+  await testErrorMessage(page, 'input#dataset.govuk-radios__input', 'Please select a dataset')
 })
 
 test('when the user clicks continue on the file upload page without selecting a file, the page correctly indicates there\'s an error', async ({ page }) => {
@@ -66,17 +44,63 @@ test('when the user clicks continue on the file upload page without selecting a 
   await page.getByRole('button', { name: 'Continue' }).click()
   await page.waitForSelector('input#datafile.govuk-file-upload')
 
-  const errorLink = await page.getByRole('link', { name: 'Please select a file' })
-  const fieldError = await page.getByText('Error: Please select a file')
+  await testErrorMessage(page, 'input#datafile.govuk-file-upload', 'Please select a file to upload')
+})
+
+test('when the user clicks continue on the email page without entering a valid email, the page correctly indicates there\'s an error', async ({ page }) => {
+  await page.goto('/')
+  // start page
+  await page.getByRole('button', { name: 'Start now' }).click()
+
+  // data subject page
+  await page.getByLabel('Conservation area').check()
+  await page.getByRole('button', { name: 'Continue' }).click()
+
+  // dataset page
+  await page.getByLabel('Conservation area dataset').check()
+  await page.getByRole('button', { name: 'Continue' }).click()
+
+  // file upload page
+  await page.getByRole('button', { name: 'Continue' }).click()
+  await page.waitForSelector('input#datafile.govuk-file-upload')
+
+  
+  let fileChooserPromise = page.waitForEvent('filechooser')
+  await page.getByText('Upload data').click()
+  let fileChooser = await fileChooserPromise
+  await fileChooser.setFiles('test/testData/conservation-area-ok.csv')
+
+  await page.getByRole('button', { name: 'Continue' }).click()
+
+  await page.waitForURL('**/no-errors')
+  await page.getByRole('button', { name: 'Continue' }).click()
+  
+  await page.waitForURL('**/email-address')
+  
+  await page.getByRole('button', { name: 'Continue' }).click()
+
+  await testErrorMessage(page, 'input#email-address.govuk-input', 'Please enter a valid email address')
+
+  await page.getByLabel('Your email address').fill('invalidEmail1')
+  await page.getByRole('button', { name: 'Continue' }).click()
+
+  await testErrorMessage(page, 'input#email-address.govuk-input', 'Please enter a valid email address')
+})
+
+
+
+
+const testErrorMessage = async (page, fieldName, expectedErrorMessage) => {
+  const errorLink = await page.getByRole('link', { name: expectedErrorMessage })
+  const fieldError = await page.getByText(`Error: ${expectedErrorMessage}`)
   const errorSummary = await page.getByText('There is a problem')
 
   expect(await errorSummary.isVisible(), 'Page should show the error summary').toBeTruthy()
   expect(await errorLink.isVisible(), 'Page should the error message that is a link to the problem field').toBeTruthy()
   expect(await fieldError.isVisible(), 'Page should show the error message next to the problem field').toBeTruthy()
   await errorLink.click()
-
-  const problemFieldIsFocused = await page.$eval('input#datafile.govuk-file-upload', (el) => el === document.activeElement)
+  const problemFieldIsFocused = await page.$eval(fieldName, (el) => el === document.activeElement)
   expect(problemFieldIsFocused, 'The focus should be on the problem field').toBeTruthy()
 
   expect(await page.title(), 'Page title should indicate there\'s an error').toMatch(/Error: .*/)
-})
+}
