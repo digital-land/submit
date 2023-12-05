59c1c58b0849bc951422d37c4e427c77a350dd6e
George Goodall <ggoodall@scottlogic.com>
2023-12-05 11:32:26 +0000

    log page requests and include sessionId and ipAddress

--- src/controllers/MyController.js
+++ src/controllers/MyController.js
@@ -1,10 +1,22 @@
 import hmpoFormWizard from 'hmpo-form-wizard'
+import logger from '../utils/logger.js'
 const { Controller } = hmpoFormWizard
 
 class MyController extends Controller {
   locals (req, res, callback) {
     req.form.options.lastPage = req.journeyModel.get('lastVisited')
     super.locals(req, res, callback)
+  }
+
+  get (req, res) {
+    logger.info({
+      type: 'PageView',
+      endpoint: req.originalUrl,
+      message: `page view occurred for page: ${req.originalUrl}`,
+      sessionId: req.sessionID,
+      ipAddress: req.ip
+    })
+    super.get(req, res)
   }
 }
 
