be9b0b2f654d3723661813f3ae7b7dee02fe62a7
George Goodall <ggoodall@scottlogic.com>
2023-12-21 14:34:42 +0000

    added test for errorAggregation function

--- test/unit/errorsController.test.js
+++ test/unit/errorsController.test.js
@@ -97,4 +97,65 @@ describe('ErrorsController', () => {
 
     expect(req.form).toEqual(expectedFormValues)
   })
+
+  describe('getAggregatedErrors', () => {
+    it('returns the correct values', () => {
+      const expectedAggregatedIssues = {
+        1: {
+          'document-URL': {
+            issue: false,
+            value: 'https://www.camden.gov.uk/camden-square-conservation-area-appraisal-and-management-strategy'
+          },
+          'end-date': {
+            issue: false,
+            value: ''
+          },
+          geometry: {
+            issue: false,
+            value: 'POLYGON ((-0.125888391245 51.54316508186, -0.125891457623 51.543177267548, -0.125903428774 51.54322160042))'
+          },
+          legislation: {
+            issue: false,
+            value: ''
+          },
+          name: {
+            issue: false,
+            value: 'Camden Square'
+          },
+          notes: {
+            issue: false,
+            value: ''
+          },
+          point: {
+            issue: false,
+            value: 'POINT (-0.130484959448 51.544845663239)'
+          },
+          'start-date': {
+            issue: {
+              type: 'invalid-value',
+              description: 'invalid-value'
+            },
+            value: '40/04/1980'
+          }
+        }
+      }
+      const expectedIssueCounts = {
+        'Start date_invalid-value': {
+          count: 1,
+          description: 'invalid-value',
+          type: 'invalid-value',
+          field: 'Start date'
+        }
+      }
+      const expectedMissingColumns = [
+        'reference'
+      ]
+
+      const { aggregatedIssues, issueCounts, missingColumns } = errorsController.getAggregatedErrors(mockApiValue)
+
+      expect(aggregatedIssues).toEqual(expectedAggregatedIssues)
+      expect(issueCounts).toEqual(expectedIssueCounts)
+      expect(missingColumns).toEqual(expectedMissingColumns)
+    })
+  })
 })
