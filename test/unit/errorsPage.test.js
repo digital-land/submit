4f2c06346503dab64ad059b0d57f72b8d85a8509
George Goodall <ggoodall@scottlogic.com>
2023-12-21 10:12:18 +0000

    fixed tests

--- test/unit/errorsPage.test.js
+++ test/unit/errorsPage.test.js
@@ -76,14 +76,18 @@ describe('errors page', () => {
           }
         ],
         issueCounts: {
-          geography: 1
+          'geography_fake error': {
+            count: 1,
+            type: 'fake error',
+            field: 'Geometry'
+          }
         },
         dataset: 'Dataset test'
       }
     }
     const html = nunjucks.render('errors.html', params).replace(/(\r\n|\n|\r)/gm, '').replace(/\t/gm, '').replace(/\s+/g, ' ')
 
-    expect(html).toContain('<li>1 issue, relating to geography</li>')
+    expect(html).toContain('<li>1 fake error issue, relating to column Geometry</li>')
     expect(html).toContain('<span class="govuk-caption-l"> Dataset test </span>')
     expect(html).toContain('<td class="govuk-table__cell app-wrap"> <div class="govuk-inset-text app-inset-text---error"> <p class="app-inset-text__value">POLYGON ((-0.125888391245 51.54316508186, -0.125891457623 51.543177267548, -0.125903428774 51.54322160042))</p> <p class="app-inset-text__error">fake error</p></div> </td>')
   })
