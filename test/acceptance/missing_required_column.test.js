b52cdb2d2f683f29485096335b828ee16f5de6d7
George Goodall <ggoodall@scottlogic.com>
2024-01-05 14:58:21 +0000

    added some further testing

---
+++ test/acceptance/missing_required_column.test.js
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+
+// a playwright test that uploads a file with a missing required column, and checks that the correct error message is displayed
+test('when the user uploads a file with a missing required column, the page correctly indicates there\'s an error', async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('button', { name: 'Start now' }).click()
+
+    await page.waitForURL('**/dataset')
+
+    await page.getByLabel('Conservation area dataset').check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await page.waitForURL('**/upload')
+
+    let fileChooserPromise = page.waitForEvent('filechooser')
+    await page.getByText('Upload data').click()
+    let fileChooser = await fileChooserPromise
+    await fileChooser.setFiles('test/testData/conservation-area-errors.csv')
+
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await page.waitForURL('**/errors')
+
+    expect(await page.title()).toBe('Your data has errors - Publish planning and housing data for England')
+
+    expect(await page.textContent('.govuk-list')).toContain('Missing required column reference')
+})
---
+++ test/unit/validationLookup.test.js
@@ -0,0 +1,16 @@
+
+
+// test the validation lookup function
+
+import { describe, it, expect } from 'vitest'
+import validationMessageLookup from '../../src/filters/validationMessageLookup.js'
+
+describe('validationMessageLookup', () => {
+    it('returns the correct message for a given field and type', () => {
+        expect(validationMessageLookup('email-address', 'required')).toBe('Enter an email address')
+    })
+    
+    it('throws an error if the field or type is not found', () => {
+        expect(() => validationMessageLookup('email-address', 'missing')).toThrow('No validation message found for field email-address and type missing')
+    })
+})
