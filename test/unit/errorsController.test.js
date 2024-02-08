44269843a8c2b616cfd9e33c69672066591053ae
George Goodall <ggoodall@scottlogic.com>
2024-02-08 14:42:59 +0000

    added additional test for checking two columns mapping to one field

--- test/unit/errorsController.test.js
+++ test/unit/errorsController.test.js
@@ -139,5 +139,55 @@ describe('ErrorsController', () => {
 
       expect(aggregatedIssues).toEqual(expectedAggregatedIssues)
     })
+
+    it('returns the correct values when a mapped column and a column with the same name exist', () => {
+      const mockApiValueModified = {...mockApiValue}
+
+      mockApiValueModified['converted-csv'][0]['start-date'] = 'this should be discarded as another column maps to start-date'
+
+      const expectedAggregatedIssues = {
+        1: {
+          'document-URL': {
+            issue: false,
+            value: 'https://www.camden.gov.uk/camden-square-conservation-area-appraisal-and-management-strategy'
+          },
+          'end-date': {
+            issue: false,
+            value: ''
+          },
+          geometry: {
+            issue: false,
+            value: 'POLYGON ((-0.125888391245 51.54316508186, -0.125891457623 51.543177267548, -0.125903428774 51.54322160042))'
+          },
+          legislation: {
+            issue: false,
+            value: ''
+          },
+          name: {
+            issue: false,
+            value: 'Camden Square'
+          },
+          notes: {
+            issue: false,
+            value: ''
+          },
+          point: {
+            issue: false,
+            value: 'POINT (-0.130484959448 51.544845663239)'
+          },
+          'start-date': {
+            issue: {
+              type: 'invalid-value',
+              description: 'invalid-value'
+            },
+            value: '40/04/1980'
+          }
+        }
+      }
+
+      const { aggregatedIssues } = errorsController.getAggregatedErrors(mockApiValueModified)
+
+      expect(aggregatedIssues).toEqual(expectedAggregatedIssues)
+    })
   })
 })
