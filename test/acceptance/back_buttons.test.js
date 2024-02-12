c160ad0cf7eee9b88189e644d2d17832d3c21a75
George Goodall <ggoodall@scottlogic.com>
2024-02-12 16:15:00 +0000

    added back button tests for the new pages

--- test/acceptance/back_buttons.test.js
+++ test/acceptance/back_buttons.test.js
@@ -10,6 +10,15 @@ test.describe('Back buttons work as expected without js for', () => {
     expect(page.url()).toBe(baseURL + '/')
   })
 
+  test('upload method page', async ({ page, baseURL }) => {
+    await page.goto('/')
+    await page.click('text=Start now')
+    await page.getByLabel('Article 4 direction area dataset').check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+    await page.getByRole('link', { name: 'Back', exact: true }).click()
+    expect(page.url()).toBe(baseURL + '/dataset')
+  })
+
   test('upload file page', async ({ page, baseURL }) => {
     await page.goto('/')
     await page.click('text=Start now')
@@ -18,6 +27,26 @@ test.describe('Back buttons work as expected without js for', () => {
     await page.getByRole('button', { name: 'Continue' }).click()
 
     await page.getByLabel('File Upload').check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await page.getByRole('link', { name: 'Back', exact: true }).click()
+    expect(page.url()).toBe(baseURL + '/upload-method')
+
+    await page.getByRole('link', { name: 'Back', exact: true }).click()
+    expect(page.url()).toBe(baseURL + '/dataset')
+
+    await page.getByRole('link', { name: 'Back', exact: true }).click()
+    expect(page.url()).toBe(baseURL + '/')
+  })
+
+  test('upload url page', async ({ page, baseURL }) => {
+    await page.goto('/')
+    await page.click('text=Start now')
+
+    await page.getByLabel('Article 4 direction area dataset').check()
+    await page.getByRole('button', { name: 'Continue' }).click()
+
+    await page.getByLabel('URL').check()
     await page.getByRole('button', { name: 'Continue' }).click()
 
     await page.getByRole('link', { name: 'Back', exact: true }).click()
