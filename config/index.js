a0e6efc2aec4dc9a79aae35bfcd5bcea385b960f
George Goodall <ggoodall@scottlogic.com>
2024-02-14 15:06:58 +0000

    add logic to handle loading correct config file

--- config/index.js
+++ config/index.js
@@ -1,5 +1,15 @@
 import HmpoConfig from 'hmpo-config'
 
 const config = new HmpoConfig()
+
+// add default config
 config.addFile('./config/default.yaml')
+
+// add environment specific config
+try{
+    config.addFile('./config/' + process.env.NODE_ENV + '.yaml')
+}catch(err){
+    console.error('No environment specific config file found for ' + process.env.NODE_ENV + '.yaml')
+}
+
 export default config.toJSON()
