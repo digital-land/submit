20edc6b2692da1e69fbacdf2eb97aba25cab3de5
George Goodall <ggoodall@scottlogic.com>
2024-02-15 12:16:23 +0000

    linting

--- src/filters/filters.js
+++ src/filters/filters.js
@@ -6,10 +6,10 @@ import prettifyColumnName from './prettifyColumnName.js'
 const { govukMarkdown } = xGovFilters
 
 const addFilters = (nunjucksEnv) => {
-    nunjucksEnv.addFilter('govukMarkdown', govukMarkdown)
-    nunjucksEnv.addFilter('validationMessageLookup', validationMessageLookup)
-    nunjucksEnv.addFilter('toErrorList', toErrorList)
-    nunjucksEnv.addFilter('prettifyColumnName', prettifyColumnName)
+  nunjucksEnv.addFilter('govukMarkdown', govukMarkdown)
+  nunjucksEnv.addFilter('validationMessageLookup', validationMessageLookup)
+  nunjucksEnv.addFilter('toErrorList', toErrorList)
+  nunjucksEnv.addFilter('prettifyColumnName', prettifyColumnName)
 }
 
 export default addFilters
