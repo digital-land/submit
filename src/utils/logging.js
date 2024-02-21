06af99ac6951fd369f44711e5862cd91aa8f4f30
George Goodall <ggoodall@scottlogic.com>
2024-02-21 14:16:44 +0000

    linting

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -2,12 +2,12 @@ import logger from '../utils/logger.js'
 import hash from '../utils/hasher.js'
 
 const logPageView = async (route, sessionID) => {
-    logger.info({
-        type: 'PageView',
-        pageRoute: route,
-        message: `page view occurred for page: ${route}`,
-        sessionId: await hash(sessionID)
-    })
+  logger.info({
+    type: 'PageView',
+    pageRoute: route,
+    message: `page view occurred for page: ${route}`,
+    sessionId: await hash(sessionID)
+  })
 }
 
-export {logPageView}
+export { logPageView }
