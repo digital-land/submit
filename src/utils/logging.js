6150ee9bfd8592c92e5e1a569b53e861e4b61e11
George Goodall <ggoodall@scottlogic.com>
2024-02-26 15:34:05 +0000

    fix logging

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,14 +1,13 @@
 import logger from '../utils/logger.js'
 import hash from '../utils/hasher.js'
 
-const logPageView = (route, sessionID) => {
-  return hash(sessionID).then(hashedSessionID => {
-    logger.info({
-      type: 'PageView',
-      pageRoute: route,
-      message: `page view occurred for page: ${route}`,
-      sessionId: hashedSessionID
-    })
+const logPageView = (route, sessionID, ipAddress) => {
+  logger.info({
+    type: 'PageView',
+    pageRoute: route,
+    message: `page view occurred for page: ${route}`,
+    sessionId: hash(sessionID),
+    ipAddress: hash(ipAddress)
   })
 }
 
