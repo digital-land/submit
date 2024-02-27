a98dc2763d698a469cb31ce7011dd18ce209ae5a
George Goodall <ggoodall@scottlogic.com>
2024-02-27 12:15:51 +0000

    lint fix

--- src/utils/logging.js
+++ src/utils/logging.js
@@ -1,13 +1,12 @@
 import logger from '../utils/logger.js'
 import hash from '../utils/hasher.js'
 
-
 const logPageView = (route, sessionID) => {
   logger.info({
     type: 'PageView',
     pageRoute: route,
     message: `page view occurred for page: ${route}`,
-    sessionId: hash(sessionID),
+    sessionId: hash(sessionID)
   })
 }
 
