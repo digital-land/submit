82c8ad3b9cff66c19298b3208bda5a13da88690b
George Goodall <ggoodall@scottlogic.com>
2024-02-29 13:30:32 +0000

    added some more tests ready for development

---
+++ test/acceptance/no_errors_page.test.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+
+
+test('the page renders a table with the correct data', async ({ page }) => {
+    expect(false).toBe(true)
+})
+
+test('the page renders a map with the correct data', async ({ page }) => {
+    expect(false).toBe(true)
+})
