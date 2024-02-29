f6812a80a39fc7fa4543db0cf26ecb9ea0494cfa
George Goodall <ggoodall@scottlogic.com>
2024-02-29 13:30:39 +0000

    linting

--- test/acceptance/no_errors_page.test.js
+++ test/acceptance/no_errors_page.test.js
@@ -1,11 +1,9 @@
 import { test, expect } from '@playwright/test'
 
-
-
 test('the page renders a table with the correct data', async ({ page }) => {
-    expect(false).toBe(true)
+  expect(false).toBe(true)
 })
 
 test('the page renders a map with the correct data', async ({ page }) => {
-    expect(false).toBe(true)
+  expect(false).toBe(true)
 })
