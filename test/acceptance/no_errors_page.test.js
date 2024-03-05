4d8281af3acf0b123ad428181437b45667161202
George Goodall <ggoodall@scottlogic.com>
2024-03-05 14:20:36 +0000

    added two more unfinished tests for testing no errors radio buttons

--- test/acceptance/no_errors_page.test.js
+++ test/acceptance/no_errors_page.test.js
@@ -7,3 +7,11 @@ test('the page renders a table with the correct data', async ({ page }) => {
 test('the page renders a map with the correct data', async ({ page }) => {
   expect(false).toBe(true)
 })
+
+test('clicking the continue button when specifying the data looks correct takes you to the confirmation page', () => {
+  expect(false).toBe(true)
+})
+
+test('clicking the continue button when specifying the data looks incorrect takes you back to the upload-method page', () => {
+  expect(false).toBe(true)
+})
