9d1c6e1cf4fe6ae339b48b7acad4549c31d29612
George Goodall <ggoodall@scottlogic.com>
2024-03-22 14:04:23 +0000

    added resultsController unit tests

--- test/unit/resultsController.test.js
+++ test/unit/resultsController.test.js
@@ -1,33 +1,93 @@
-import ResultsController from '../../src/controllers/ResultsController.js'
-import { describe, it, vi, expect } from 'vitest'
+import ResultsController from '../../src/controllers/resultsController.js'
+import { describe, it, vi, expect, beforeEach } from 'vitest'
 
 describe('ResultsController', () => {
-  it('should handle complete status', async () => {
-    const controller = new ResultsController()
-    const req = { params: { result_id: 'complete_test_id' } }
-    const res = { render: vi.fn(), redirect: vi.fn() }
-    const next = vi.fn()
+  vi.mock('@/utils/publishRequestAPI.js')
+  
+  let publishRequestApi
+  let resultsController
 
-    // need to mock the api here, with wiremock?
+  const req = {
+    params: { id: 'testId' },
+    form: { options: {} }
+  }
 
-    await controller.get(req, res, next)
+  beforeEach(async () => {
+    publishRequestApi = await import('@/utils/publishRequestAPI')
 
-    // Assuming your API returns a 'complete' status for 'test_id'
-    expect(res.render).toHaveBeenCalled()
-    expect(res.redirect).not.toHaveBeenCalled()
+    resultsController = new ResultsController({
+      route: '/results'
+    })
   })
 
-  it('should redirect for non-complete status', async () => {
-    const controller = new ResultsController()
-    const req = { params: { result_id: 'processing_test_id' } }
-    const res = { render: vi.fn(), redirect: vi.fn() }
-    const next = vi.fn()
+  describe('configure', () => {
+    it('should add the result to the controller class', async () => {
 
-    // need to mock the api here
-    await controller.get(req, res, next)
+      const mockResult = { hasErrors: () => false }
+      publishRequestApi.getRequestData = vi.fn().mockResolvedValue(mockResult)
 
-    // Assuming your API returns a non-complete status for 'test_id'
-    expect(res.redirect).toHaveBeenCalledWith(`/status/${req.params.result_id}`)
-    expect(res.render).not.toHaveBeenCalled()
+      await resultsController.configure(req, {}, () => {})
+      expect(resultsController.result).toBeDefined()
+    })
+
+    it("should set the template to the 404 template if the result wasn't found", async () => {
+      const mockResult = { response: { error: { code: 404 } } }
+      publishRequestApi.getRequestData = vi.fn().mockResolvedValue(mockResult)
+
+      await resultsController.configure(req, {}, () => {})
+      expect(req.form.options.template).toBe('404')
+    })
+
+    it('should set the template to the generic error template in the result processing errored', async () => {
+      const mockResult = { response: { error: { code: 500 } } }
+      publishRequestApi.getRequestData = vi.fn().mockResolvedValue(mockResult)
+
+      await resultsController.configure(req, {}, () => {})
+      expect(req.form.options.template).toBe('500')
+    })
+
+    it('should set the template to the errors template if the result has errors', async () => {
+      const mockResult = { hasErrors: () => true }
+      publishRequestApi.getRequestData = vi.fn().mockResolvedValue(mockResult)
+
+      await resultsController.configure(req, {}, () => {})
+      expect(req.form.options.template).toBe('errors')
+    })
+
+    it('should set the template to the no-errors template if the result has no errors', async () => {
+      const mockResult = { hasErrors: () => false }
+      publishRequestApi.getRequestData = vi.fn().mockResolvedValue(mockResult)
+
+      await resultsController.configure(req, {}, () => {})
+      expect(req.form.options.template).toBe('no-errors')
+    })
+  })
+
+  describe('locals', () => {
+    it('should redirect to the status page if the result is not complete', async () => {
+      resultsController.result = { isComplete: () => false }
+      const res = { redirect: vi.fn() }
+      await resultsController.locals(req, res, () => {})
+      expect(res.redirect).toHaveBeenCalledWith(`/status/${req.params.id}`)
+    })
+
+    it('should set the result to the form options if the result is complete', async () => {
+      resultsController.result = { isComplete: () => true }
+      const res = { redirect: vi.fn() }
+      await resultsController.locals(req, res, () => {})
+      expect(req.form.options.result).toBe(resultsController.result)
+    })
+  })
+
+  describe('noErrors', () => {
+    it('should return false if the result has errors', () => {
+      resultsController.result = { hasErrors: () => true }
+      expect(resultsController.noErrors()).toBe(false)
+    })
+
+    it('should return true if the result has no errors', () => {
+      resultsController.result = { hasErrors: () => false }
+      expect(resultsController.noErrors()).toBe(true)
+    })
   })
 })
