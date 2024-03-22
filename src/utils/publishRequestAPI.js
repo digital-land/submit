b36426dd1a3837cdb725803eeb4897b6f17823a3
George Goodall <ggoodall@scottlogic.com>
2024-03-22 14:19:21 +0000

    added unit tests for publishRequestApi util file

--- src/utils/publishRequestAPI.js
+++ src/utils/publishRequestAPI.js
@@ -41,43 +41,18 @@ const postRequest = async (formData) => {
 }
 
 export const getRequestData = async (resultId) => {
-  // const result = await fetch(`${config.publishRequestApi}/results/${resultId}`);
-  const result = {
-    id: 1,
-    type: 'check_url',
-    status: 'COMPLETE',
-    created: '2024-03-13T16:50:59.472751Z',
-    modified: '2024-03-13T16:50:59.472751Z',
-    params: {
-      type: 'check_url',
-      collection: 'article_4_direction',
-      dataset: 'article_4_direction_area',
-      url: 'https://services3.arcgis.com/lCzPKKaGs7lhrnrV/ArcGIS/rest/services/Planning_Portal_Article_4_Direction_081223/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
-    },
-    response: {
-      data: {
-        'column-field-log': [],
-        'error-summary': []
-      },
-      details: [
-        {
-          'line-number': 1,
-          'converted-row': {},
-          'issue-log-row': {}
-        }
-      ]
-    }
-  }
-  return new RequestData(result)
+  const result = await fetch(`${config.publishRequestApi}/results/${resultId}`)
+  const resultJson = await result.json()
+  return new RequestData(resultJson)
 }
 
-class RequestData {
+export class RequestData {
   constructor (data) {
     Object.assign(this, data)
   }
 
   hasErrors () {
-    return this.response.data['error-summary'].length > 0
+    return this.data['error-summary'].length > 0
   }
 
   isComplete () {
---
+++ test/unit/publishRequestAPI.test.js
@@ -0,0 +1,64 @@
+import { postFileRequest, postUrlRequest, getRequestData, RequestData } from '../../src/utils/publishRequestAPI.js'
+import { test, describe, expect, beforeEach, vi } from 'vitest'
+
+describe('publishRequestApi', () => {
+  beforeEach(() => {
+    global.fetch = vi.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({ id: 'testId' })
+      })
+    )
+  })
+
+  test('postFileRequest', async ({ fetch }) => {
+    const formData = {
+      uploadedFilename: 'testFile',
+      originalFilename: 'originalTestFile',
+      dataset: 'testDataset',
+      collection: 'testCollection',
+      geomType: 'testGeomType'
+    }
+
+    const response = await postFileRequest(formData)
+
+    expect(response).toBe('testId')
+  })
+
+  test('postUrlRequest', async ({ fetch }) => {
+    const formData = {
+      url: 'https://test.com',
+      dataset: 'testDataset',
+      collection: 'testCollection',
+      geomType: 'testGeomType'
+    }
+
+    const response = await postUrlRequest(formData)
+
+    expect(response).toBe('testId')
+  })
+
+  test('getRequestData', async () => {
+    const resultId = 'testId'
+
+    const mockFetchResponse = {
+      id: 'testId',
+      data: {
+        'error-summary': []
+      },
+      status: 'COMPLETE'
+    }
+    global.fetch = vi.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockFetchResponse)
+      })
+    )
+
+    const requestData = await getRequestData(resultId)
+
+    expect(requestData).toBeInstanceOf(RequestData)
+    expect(requestData.hasErrors()).toBe(false)
+    expect(requestData.isComplete()).toBe(true)
+  })
+})
