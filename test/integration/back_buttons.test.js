55fda70af74760e2ef7d52fb9a9f0d9a2e463eff
George Goodall <ggoodall@scottlogic.com>
2024-04-09 10:34:03 +0100

    update back_button tests to use localstack test container

--- test/integration/back_buttons.test.js
+++ test/integration/back_buttons.test.js
@@ -1,18 +1,34 @@
 import { test } from '@playwright/test'
+
+import Localstack from '../testContainers/localstack'
+
+import config from '../../config/index'
+
 import StartPage from '../PageObjectModels/startPage'
 import DatasetPage from '../PageObjectModels/datasetPage'
 import GeometryTypePage from '../PageObjectModels/geometryTypePage'
 import UploadMethodPage from '../PageObjectModels/uploadMethodPage'
 import UploadFilePage from '../PageObjectModels/uploadFilePage'
 import UploadURLPage from '../PageObjectModels/uploadURLPage'
-import StatusPage from '../PageObjectModels/statusPage'
-import ErrorsPage from '../PageObjectModels/errorsPage'
-import NoErrorsPage from '../PageObjectModels/noErrorsPage'
+// import StatusPage from '../PageObjectModels/statusPage'
+// import ErrorsPage from '../PageObjectModels/errorsPage'
+// import NoErrorsPage from '../PageObjectModels/noErrorsPage'
 
-test.use({ javaScriptEnabled: false })
+let localstack
 
-test.describe('Back buttons work as expected without js for', () => {
-  test.skip('data set page', async ({ page, baseURL }) => {
+test.describe('Back buttons work as expected without js for...', () => {
+  test.use({ javaScriptEnabled: false })
+
+  test.beforeAll(async () => {
+    localstack = await new Localstack().start()
+    await localstack.createBucket(config.aws.bucket)
+  })
+
+  test.afterAll(async () => {
+    await localstack.stop()
+  })
+
+  test('data set page', async ({ page }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
 
@@ -25,7 +41,7 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  test.skip('geometry type page', async ({ page, baseURL }) => {
+  test('geometry type page', async ({ page }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
     const geometryTypePage = new GeometryTypePage(page)
@@ -46,7 +62,7 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  test.skip('upload method page', async ({ page, baseURL }) => {
+  test('upload method page', async ({ page, baseURL }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
     const uploadMethodPage = new UploadMethodPage(page)
@@ -67,7 +83,7 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  test.skip('upload method page (from geometry type)', async ({ page, baseURL }) => {
+  test('upload method page (from geometry type)', async ({ page, baseURL }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
     const geometryTypePage = new GeometryTypePage(page)
@@ -94,7 +110,7 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  test.skip('upload file page', async ({ page, baseURL }) => {
+  test('upload file page', async ({ page, baseURL }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
     const uploadMethodPage = new UploadMethodPage(page)
@@ -123,7 +139,7 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  test.skip('upload url page', async ({ page, baseURL }) => {
+  test('upload url page', async ({ page, baseURL }) => {
     const startPage = new StartPage(page)
     const datasetPage = new DatasetPage(page)
     const uploadMethodPage = new UploadMethodPage(page)
@@ -152,106 +168,110 @@ test.describe('Back buttons work as expected without js for', () => {
     await startPage.waitForPage()
   })
 
-  // ToDo: Skipping following as backend not working with urls yet
-  test.skip('status page', async ({ page, baseURL }) => {
-    const startPage = new StartPage(page)
-    const datasetPage = new DatasetPage(page)
-    const uploadMethodPage = new UploadMethodPage(page)
-    const uploadURLPage = new UploadURLPage(page)
-    const statusPage = new StatusPage(page)
+  // // ToDo: Skipping following as backend not working with urls yet
+  // test.skip('status page', async ({ page, baseURL }) => {
+  //   const startPage = new StartPage(page)
+  //   const datasetPage = new DatasetPage(page)
+  //   const uploadMethodPage = new UploadMethodPage(page)
+  //   const uploadURLPage = new UploadURLPage(page)
+  //   const statusPage = new StatusPage(page)
 
-    await startPage.navigateHere()
-    await startPage.clickStartNow()
+  //   await startPage.navigateHere()
+  //   await startPage.clickStartNow()
 
-    await datasetPage.waitForPage()
-    await datasetPage.selectDataset(DatasetPage.datasets.Article_4_direction_area_dataset)
-    await datasetPage.clickContinue()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.selectDataset(DatasetPage.datasets.Article_4_direction_area_dataset)
+  //   await datasetPage.clickContinue()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
-    await uploadMethodPage.clickContinue()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
+  //   await uploadMethodPage.clickContinue()
 
-    await uploadURLPage.waitForPage()
-    await uploadURLPage.enterURL('https://example.com/article-4-direction-area.csv')
-    await uploadURLPage.clickContinue()
+  //   await uploadURLPage.waitForPage()
+  //   await uploadURLPage.enterURL('https://example.com/article-4-direction-area.csv')
+  //   await uploadURLPage.clickContinue()
 
-    await statusPage.waitForPage()
-    await statusPage.goBack()
+  //   await statusPage.waitForPage()
+  //   await statusPage.goBack()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.goBack()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.goBack()
 
-    await datasetPage.waitForPage()
-    await datasetPage.goBack()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.goBack()
 
-    await startPage.waitForPage()
-  })
+  //   await startPage.waitForPage()
+  // })
 
-  test.skip('errors page', async ({ page, baseURL }) => {
-    const startPage = new StartPage(page)
-    const datasetPage = new DatasetPage(page)
-    const uploadMethodPage = new UploadMethodPage(page)
-    const uploadURLPage = new UploadURLPage(page)
-    const errorsPage = new ErrorsPage(page)
+  // test.skip('errors page', async ({ page, baseURL }) => {
+  //   const startPage = new StartPage(page)
+  //   const datasetPage = new DatasetPage(page)
+  //   const uploadMethodPage = new UploadMethodPage(page)
+  //   const uploadURLPage = new UploadURLPage(page)
+  //   const errorsPage = new ErrorsPage(page)
 
-    await startPage.navigateHere()
-    await startPage.clickStartNow()
+  //   await startPage.navigateHere()
+  //   await startPage.clickStartNow()
 
-    await datasetPage.waitForPage()
-    await datasetPage.selectDataset(DatasetPage.datasets.Conservation_area_dataset)
-    await datasetPage.clickContinue()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.selectDataset(DatasetPage.datasets.Conservation_area_dataset)
+  //   await datasetPage.clickContinue()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
-    await uploadMethodPage.clickContinue()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
+  //   await uploadMethodPage.clickContinue()
 
-    await uploadURLPage.waitForPage()
-    await uploadURLPage.enterURL('https://example.com/conservation-area-errors.csv')
-    await uploadURLPage.clickContinue()
+  //   await uploadURLPage.waitForPage()
+  //   await uploadURLPage.enterURL('https://example.com/conservation-area-errors.csv')
+  //   await uploadURLPage.clickContinue()
 
-    await errorsPage.waitForPage()
-    await errorsPage.goBack()
+  //   await errorsPage.waitForPage()
+  //   await errorsPage.goBack()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.goBack()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.goBack()
 
-    await datasetPage.waitForPage()
-    await datasetPage.goBack()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.goBack()
 
-    await startPage.waitForPage()
-  })
+  //   await startPage.waitForPage()
+  // })
 
-  test.skip('no errors page', async ({ page, baseURL }) => {
-    const startPage = new StartPage(page)
-    const datasetPage = new DatasetPage(page)
-    const uploadMethodPage = new UploadMethodPage(page)
-    const uploadURLPage = new UploadURLPage(page)
-    const noErrorsPage = new NoErrorsPage(page)
+  // test.skip('no errors page', async ({ page, baseURL }) => {
+  //   const startPage = new StartPage(page)
+  //   const datasetPage = new DatasetPage(page)
+  //   const uploadMethodPage = new UploadMethodPage(page)
+  //   const uploadURLPage = new UploadURLPage(page)
+  //   const noErrorsPage = new NoErrorsPage(page)
 
-    await startPage.navigateHere()
-    await startPage.clickStartNow()
+  //   await startPage.navigateHere()
+  //   await startPage.clickStartNow()
 
-    await datasetPage.waitForPage()
-    await datasetPage.selectDataset(DatasetPage.datasets.Conservation_area_dataset)
-    await datasetPage.clickContinue()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.selectDataset(DatasetPage.datasets.Conservation_area_dataset)
+  //   await datasetPage.clickContinue()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
-    await uploadMethodPage.clickContinue()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
+  //   await uploadMethodPage.clickContinue()
 
-    await uploadURLPage.waitForPage()
-    await uploadURLPage.enterURL('https://example.com/conservation-area-ok.csv')
-    await uploadURLPage.clickContinue()
+  //   await uploadURLPage.waitForPage()
+  //   await uploadURLPage.enterURL('https://example.com/conservation-area-ok.csv')
+  //   await uploadURLPage.clickContinue()
 
-    await noErrorsPage.waitForPage()
-    await noErrorsPage.goBack()
+  //   await noErrorsPage.waitForPage()
+  //   await noErrorsPage.goBack()
 
-    await uploadMethodPage.waitForPage()
-    await uploadMethodPage.goBack()
+  //   await uploadMethodPage.waitForPage()
+  //   await uploadMethodPage.goBack()
 
-    await datasetPage.waitForPage()
-    await datasetPage.goBack()
+  //   await datasetPage.waitForPage()
+  //   await datasetPage.goBack()
 
-    await startPage.waitForPage()
-  })
+  //   await startPage.waitForPage()
+  // })
+
+  // test.skip('confirmation page', async ({ page, baseURL }) => {
+
+  // })
 })
