d70419077ef4af4b69fd967b050b2fdc1aa4f593
George Goodall <ggoodall@scottlogic.com>
2024-04-11 16:29:15 +0100

    update request check to use new POM naming

--- test/acceptance/request_check.test.js
+++ test/acceptance/request_check.test.js
@@ -6,86 +6,49 @@
         - request check of a datafile when javascript fails
 */
 
-import { test, expect } from '@playwright/test'
-import { LocalstackContainer } from '@testcontainers/localstack'
-import { S3Client, CreateBucketCommand, HeadBucketCommand } from '@aws-sdk/client-s3'
+import { test } from '@playwright/test'
 
-import StartPOM from './PageObjectModels/startPOM'
-import DatasetPOM from './PageObjectModels/datasetPOM'
-import UploadMethodPOM from './PageObjectModels/uploadMethodPOM'
-import UploadFilePOM from './PageObjectModels/uploadFilePOM'
-import StatusPOM from './PageObjectModels/statusPOM'
-import ResultsPOM from './PageObjectModels/resultsPOM'
-
-test.describe('initial test container tests', async () => {
-  let container
-
-  test.beforeAll(async () => {
-    container = await new LocalstackContainer('localstack/localstack:latest').start()
-  })
-
-  test.afterAll(async () => {
-    await container.stop()
-  })
-
-  test.skip('should create a S3 bucket', async () => {
-    const client = new S3Client({
-      endpoint: container.getConnectionUri(),
-      forcePathStyle: true,
-      region: 'us-east-1',
-      credentials: {
-        secretAccessKey: 'test',
-        accessKeyId: 'test'
-      }
-    })
-    const input = {
-      Bucket: 'testcontainers'
-    }
-    const command = new CreateBucketCommand(input)
-
-    const createBucketResponse = await client.send(command)
-    expect(createBucketResponse.$metadata.httpStatusCode).toEqual(200)
-    const headBucketResponse = await client.send(new HeadBucketCommand(input))
-    expect(headBucketResponse.$metadata.httpStatusCode).toEqual(200)
-
-    await container.stop()
-  })
-})
+import StartPage from './PageObjectModels/startPage'
+import DatasetPage from './PageObjectModels/datasetPage'
+import UploadMethodPage from './PageObjectModels/uploadMethodPage'
+import UploadFilePage from './PageObjectModels/uploadFilePage'
+import StatusPage from './PageObjectModels/statusPage'
+import ResultsPage from './PageObjectModels/resultsPage'
 
 test.describe('Request Check', () => {
   test.describe('with javascript enabled', () => {
     test.skip('request check of a datafile', async ({ page }) => {
-      const startPOM = new StartPOM(page)
-      const datasetPOM = new DatasetPOM(page)
-      const uploadMethodPOM = new UploadMethodPOM(page)
-      const uploadFilePOM = new UploadFilePOM(page)
-      const statusPOM = new StatusPOM(page)
-      const resultsPOM = new ResultsPOM(page)
+      const startPage = new StartPage(page)
+      const datasetPage = new DatasetPage(page)
+      const uploadMethodPage = new UploadMethodPage(page)
+      const uploadFilePage = new UploadFilePage(page)
+      const statusPage = new StatusPage(page)
+      const resultsPage = new ResultsPage(page)
 
-      await startPOM.navigateHere()
-      await startPOM.clickStartNow()
+      await startPage.navigateHere()
+      await startPage.clickStartNow()
 
-      await datasetPOM.waitForPage()
-      await datasetPOM.selectDataset(DatasetPOM.datasets.Article_4_direction_area_dataset)
-      await datasetPOM.clickContinue()
+      await datasetPage.waitForPage()
+      await datasetPage.selectDataset(DatasetPage.datasets.Article_4_direction_area_dataset)
+      await datasetPage.clickContinue()
 
-      await uploadMethodPOM.waitForPage()
-      await uploadMethodPOM.selectUploadMethod(UploadMethodPOM.uploadMethods.File)
-      await uploadMethodPOM.clickContinue()
+      await uploadMethodPage.waitForPage()
+      await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.File)
+      await uploadMethodPage.clickContinue()
 
-      await uploadFilePOM.waitForPage()
-      await uploadFilePOM.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
-      await uploadFilePOM.clickContinue()
+      await uploadFilePage.waitForPage()
+      await uploadFilePage.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
+      await uploadFilePage.clickContinue()
 
-      await statusPOM.waitForPage()
-      await statusPOM.expectStatusToBe('NEW')
-      await statusPOM.expectStatusToBe('IN_PROGRESS')
-      await statusPOM.waitForContinueButton()
-      await statusPOM.expectStatusToBe('COMPLETE')
-      await statusPOM.clickContinue()
+      await statusPage.waitForPage()
+      await statusPage.expectStatusToBe('NEW')
+      await statusPage.expectStatusToBe('IN_PROGRESS')
+      await statusPage.waitForContinueButton()
+      await statusPage.expectStatusToBe('COMPLETE')
+      await statusPage.clickContinue()
 
-      await resultsPOM.waitForPage()
-      await resultsPOM.expectPageIsNoErrorsPage()
+      await resultsPage.waitForPage()
+      await resultsPage.expectPageIsNoErrorsPage()
     })
   })
 
@@ -93,38 +56,38 @@ test.describe('Request Check', () => {
     test.use({ javaScriptEnabled: false })
 
     test.skip('request check of a datafile', async ({ page }) => {
-      const startPOM = new StartPOM(page)
-      const datasetPOM = new DatasetPOM(page)
-      const uploadMethodPOM = new UploadMethodPOM(page)
-      const uploadFilePOM = new UploadFilePOM(page)
-      const statusPOM = new StatusPOM(page)
-      const resultsPOM = new ResultsPOM(page)
+      const startPage = new StartPage(page)
+      const datasetPage = new DatasetPage(page)
+      const uploadMethodPage = new UploadMethodPage(page)
+      const uploadFilePage = new UploadFilePage(page)
+      const statusPage = new StatusPage(page)
+      const resultsPage = new ResultsPage(page)
 
-      await startPOM.navigateHere()
-      await startPOM.clickStartNow()
+      await startPage.navigateHere()
+      await startPage.clickStartNow()
 
-      await datasetPOM.waitForPage()
-      await datasetPOM.selectDataset(DatasetPOM.datasets.Article_4_direction_area_dataset)
-      await datasetPOM.clickContinue()
+      await datasetPage.waitForPage()
+      await datasetPage.selectDataset(DatasetPage.datasets.Article_4_direction_area_dataset)
+      await datasetPage.clickContinue()
 
-      await uploadMethodPOM.waitForPage()
-      await uploadMethodPOM.selectUploadMethod(UploadMethodPOM.uploadMethods.File)
-      await uploadMethodPOM.clickContinue()
+      await uploadMethodPage.waitForPage()
+      await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.File)
+      await uploadMethodPage.clickContinue()
 
-      await uploadFilePOM.waitForPage()
-      await uploadFilePOM.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
-      await uploadFilePOM.clickContinue()
+      await uploadFilePage.waitForPage()
+      await uploadFilePage.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
+      await uploadFilePage.clickContinue()
 
-      await statusPOM.waitForPage()
-      await statusPOM.expectStatusToBe('NEW')
-      await statusPOM.waitForContinueButton()
-      await statusPOM.clickContinue()
+      await statusPage.waitForPage()
+      await statusPage.expectStatusToBe('NEW')
+      await statusPage.waitForContinueButton()
+      await statusPage.clickContinue()
 
-      await resultsPOM.waitForPage()
+      await resultsPage.waitForPage()
 
       // could be redirected back to the status page here? or could remain on  the results page
 
-      await statusPOM.waitForPage()
+      await statusPage.waitForPage()
     })
   })
 
@@ -132,37 +95,37 @@ test.describe('Request Check', () => {
     test.skip('request check of a datafile', async ({ page }) => {
       // we need a way to make javascript fail here.
 
-      const startPOM = new StartPOM(page)
-      const datasetPOM = new DatasetPOM(page)
-      const uploadMethodPOM = new UploadMethodPOM(page)
-      const uploadFilePOM = new UploadFilePOM(page)
-      const statusPOM = new StatusPOM(page)
-      const resultsPOM = new ResultsPOM(page)
+      const startPage = new StartPage(page)
+      const datasetPage = new DatasetPage(page)
+      const uploadMethodPage = new UploadMethodPage(page)
+      const uploadFilePage = new UploadFilePage(page)
+      const statusPage = new StatusPage(page)
+      const resultsPage = new ResultsPage(page)
 
-      await startPOM.navigateHere()
-      await startPOM.clickStartNow()
+      await startPage.navigateHere()
+      await startPage.clickStartNow()
 
-      await datasetPOM.waitForPage()
-      await datasetPOM.selectDataset(DatasetPOM.datasets.Article_4_direction_area_dataset)
-      await datasetPOM.clickContinue()
+      await datasetPage.waitForPage()
+      await datasetPage.selectDataset(DatasetPage.datasets.Article_4_direction_area_dataset)
+      await datasetPage.clickContinue()
 
-      await uploadMethodPOM.waitForPage()
-      await uploadMethodPOM.selectUploadMethod(UploadMethodPOM.uploadMethods.File)
-      await uploadMethodPOM.clickContinue()
+      await uploadMethodPage.waitForPage()
+      await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.File)
+      await uploadMethodPage.clickContinue()
 
-      await uploadFilePOM.waitForPage()
-      await uploadFilePOM.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
-      await uploadFilePOM.clickContinue()
+      await uploadFilePage.waitForPage()
+      await uploadFilePage.uploadFile('test/datafiles/Article_4_direction_area_dataset.csv')
+      await uploadFilePage.clickContinue()
 
-      await statusPOM.waitForPage()
-      await statusPOM.expectStatusToBe('NEW')
-      await statusPOM.expectStatusToBe('IN_PROGRESS')
-      await statusPOM.waitForContinueButton()
-      await statusPOM.expectStatusToBe('FAILED')
-      await statusPOM.clickContinue()
+      await statusPage.waitForPage()
+      await statusPage.expectStatusToBe('NEW')
+      await statusPage.expectStatusToBe('IN_PROGRESS')
+      await statusPage.waitForContinueButton()
+      await statusPage.expectStatusToBe('FAILED')
+      await statusPage.clickContinue()
 
-      await resultsPOM.waitForPage()
-      await resultsPOM.expectPageIsErrorPage()
+      await resultsPage.waitForPage()
+      await resultsPage.expectPageIsErrorPage()
     })
   })
 })
