8ffb4e1450a66f06ebf5b3070316771db53fde55
George Goodall <ggoodall@scottlogic.com>
2024-04-11 15:57:04 +0100

    add checks for when the session is valid

--- test/integration/pages_load_ok.test.js
+++ test/integration/pages_load_ok.test.js
@@ -1,12 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+import StartPage from '../PageObjectModels/startPage'
+import DatasetPage from '../PageObjectModels/datasetPage'
+import GeometryTypePage from '../PageObjectModels/geometryTypePage'
+import UploadMethodPage from '../PageObjectModels/uploadMethodPage'
 import StatusPage from '../PageObjectModels/statusPage'
 import ResultsPage from '../PageObjectModels/resultsPage'
 
-const checkRouteResponse = async (page, route, status) => {
+const checkRouteResponse = async (page, route, statuses) => {
   const response = await page.goto(route)
-  expect(response.ok()).toBe(true)
-  expect(response.status()).toBe(status)
+  if (Array.isArray(statuses)) {
+    expect(statuses.includes(response.status())).toBeTruthy()
+  } else {
+    expect(response.status()).toBe(statuses)
+  }
 }
 
 const checkSessionExpired = async (page, route) => {
@@ -15,21 +22,18 @@ const checkSessionExpired = async (page, route) => {
   await expect(sessionExpiredText !== undefined).toBeTruthy()
 }
 
-test('start page loads ok', async ({ page }) => {
-  await checkRouteResponse(page, '/', 200)
-})
-
-test.describe('without a valid session, the user can not access the later pages', () => {
-  test('/upload-method', async ({ page }) => {
-    await checkSessionExpired(page, '/upload-method')
-  })
-
+test.describe('without a valid session, the user can not access the later form pages', () => {
+  
   test('/dataset', async ({ page }) => {
     await checkSessionExpired(page, '/dataset')
   })
-
+  
   test('/geometry-type', async ({ page }) => {
     await checkSessionExpired(page, '/geometry-type')
+  })
+  
+  test('/upload-method', async ({ page }) => {
+    await checkSessionExpired(page, '/upload-method')
   })
 
   test('/upload', async ({ page }) => {
@@ -39,6 +43,102 @@ test.describe('without a valid session, the user can not access the later pages'
   test('/url', async ({ page }) => {
     await checkSessionExpired(page, '/url')
   })
+})
+
+test.describe('with a valid session, the user can access the later form pages', () => {
+  test('/dataset', async ({ page }) => {
+    const startPage = new StartPage(page)
+
+    await startPage.navigateHere()
+    await startPage.clickStartNow()
+
+    await checkRouteResponse(page, '/dataset', [200, 304])
+  })
+
+  test('/geometry-type', async ({ page }) => {
+    const startPage = new StartPage(page)
+    const datasetPage = new DatasetPage(page)
+
+    await startPage.navigateHere()
+    await startPage.clickStartNow()
+
+    await datasetPage.waitForPage()
+    await datasetPage.selectDataset(DatasetPage.datasets.Tree)
+    await datasetPage.clickContinue()
+
+    await checkRouteResponse(page, '/geometry-type', [200, 304])
+  })
+
+  test('/upload-method', async ({ page }) => {
+    const startPage = new StartPage(page)
+    const datasetPage = new DatasetPage(page)
+    const geometryTypePage = new GeometryTypePage(page)
+
+    await startPage.navigateHere()
+    await startPage.clickStartNow()
+
+    await datasetPage.waitForPage()
+    await datasetPage.selectDataset(DatasetPage.datasets.Tree)
+    await datasetPage.clickContinue()
+
+    await geometryTypePage.waitForPage()
+    await geometryTypePage.clickContinue()
+
+    await checkRouteResponse(page, '/upload-method', [200, 304])
+  })
+
+  test('/upload', async ({ page }) => {
+    const startPage = new StartPage(page)
+    const datasetPage = new DatasetPage(page)
+    const geometryTypePage = new GeometryTypePage(page)
+    const uploadMethodPage = new UploadMethodPage(page)
+
+    await startPage.navigateHere()
+    await startPage.clickStartNow()
+
+    await datasetPage.waitForPage()
+    await datasetPage.selectDataset(DatasetPage.datasets.Tree)
+    await datasetPage.clickContinue()
+
+    await geometryTypePage.waitForPage()
+    await geometryTypePage.selectGeometryType(GeometryTypePage.geometryTypes.point)
+    await geometryTypePage.clickContinue()
+
+    await uploadMethodPage.waitForPage()
+    await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.File)
+    await uploadMethodPage.clickContinue()
+
+    await checkRouteResponse(page, '/upload', [200, 304])
+  })
+
+  test('/url', async ({ page }) => {
+    const startPage = new StartPage(page)
+    const datasetPage = new DatasetPage(page)
+    const geometryTypePage = new GeometryTypePage(page)
+    const uploadMethodPage = new UploadMethodPage(page)
+
+    await startPage.navigateHere()
+    await startPage.clickStartNow()
+
+    await datasetPage.waitForPage()
+    await datasetPage.selectDataset(DatasetPage.datasets.Tree)
+    await datasetPage.clickContinue()
+
+    await geometryTypePage.waitForPage()
+    await geometryTypePage.clickContinue()
+
+    await geometryTypePage.waitForPage()
+    await geometryTypePage.selectGeometryType(GeometryTypePage.geometryTypes.point)
+    await geometryTypePage.clickContinue()
+
+    await uploadMethodPage.waitForPage()
+    await uploadMethodPage.selectUploadMethod(UploadMethodPage.uploadMethods.URL)
+    await uploadMethodPage.clickContinue()
+
+    await checkRouteResponse(page, '/url', [200, 304])
+  })
+
+  
 })
 
 // ToDo: Complete these tests
