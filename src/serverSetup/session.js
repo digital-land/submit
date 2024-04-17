1862c558dd0df8dd4ff865b6567deafe8b34f2d4
George Goodall <ggoodall@scottlogic.com>
2024-04-17 14:54:08 +0100

    added some logging

--- src/serverSetup/session.js
+++ src/serverSetup/session.js
@@ -8,6 +8,10 @@ import logger from '../utils/logger.js'
 export function setupSession (app) {
   app.use(cookieParser())
 
+  console.log('using config for redis: ' + config.environment)
+  console.log('redis host: ' + config.redis.host)
+  console.log('redis port: ' + config.redis.port)
+
   const redisClient = createClient({
     host: config.redis.host,
     port: config.redis.port
