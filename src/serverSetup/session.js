9da8817093b9558bf72532fb603000f02d6f04f6
George Goodall <ggoodall@scottlogic.com>
2024-04-17 16:29:38 +0100

    if redis not set in config, use default in memory storage

--- src/serverSetup/session.js
+++ src/serverSetup/session.js
@@ -8,21 +8,24 @@ import logger from '../utils/logger.js'
 export function setupSession (app) {
   app.use(cookieParser())
 
-  const redisClient = createClient({
-    host: config.redis.host,
-    port: config.redis.port
-  })
-  redisClient.connect().catch(logger.error)
+  let sessionStore;
+  if (config.redis) {
+    const redisClient = createClient({
+      host: config.redis.host,
+      port: config.redis.port
+    })
+    redisClient.connect().catch(logger.error)
 
-  const redisStore = new RedisStore({
-    client: redisClient
-  })
+    sessionStore = new RedisStore({
+      client: redisClient
+    })
+  }
 
   app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: redisStore,
+    store: sessionStore,
     cookie: {
       secure: config.environment === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 7
