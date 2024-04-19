db8a521540807709e1215703859be6fa0001d1dd
Chris Cundill <chrisc@diligentsoft.co>
2024-04-19 14:39:59 +0100

    Backout of using secure cookie setting production for now since it requires some further investigation

--- src/serverSetup/session.js
+++ src/serverSetup/session.js
@@ -19,17 +19,13 @@ export function setupSession (app) {
       client: redisClient
     })
   }
-  // Trust first proxy otherwise secure cookies will not be returned
-  // See https://github.com/expressjs/session?tab=readme-ov-file#cookiesecure
-  // See https://expressjs.com/en/guide/behind-proxies.html
-  app.set('trust proxy', 2) // trust two proxy hops
   app.use(session({
     secret: process.env.SESSION_SECRET || 'keyboard cat',
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
-      secure: config.environment === 'production',
+      secure: false,
       maxAge: 1000 * 60 * 60 * 24 * 7
     }
   }))
