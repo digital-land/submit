a6899ac420f90c50e5536027fbdd2bdbe837e8f8
George Goodall <ggoodall@scottlogic.com>
2024-04-29 12:01:18 +0100

    fix pagination ellipsis placement when there are less than 6 pages

--- src/models/requestData.js
+++ src/models/requestData.js
@@ -211,15 +211,26 @@ const { min, max } = Math
 const range = (lo, hi) => Array.from({ length: hi - lo }, (_, i) => i + lo)
 
 const pagination = (count, current, ellipsis = '...') => {
-  if (count <= 4) {
+  if (count <= 5) {
     return range(1, count + 1)
   }
   const adjacent = 1
   const left = current === count ? current - 2 * adjacent : max(0, current - adjacent)
   const right = current === 1 ? 1 + adjacent * 2 : min(count, current + adjacent)
   const middle = range(left, right + 1)
-  const leftEllipsis = left > 1
-  const rightEllipsis = right < count
+  let leftEllipsis = left > 1
+  let rightEllipsis = right < count
+
+  if (leftEllipsis && middle[0] === 2) {
+    leftEllipsis = false
+    middle.unshift(1)
+  }
+
+  if (rightEllipsis && middle[middle.length - 1] === count - 1) {
+    rightEllipsis = false
+    middle.push(count)
+  }
+
   const result = [
     ...(leftEllipsis ? [1, ellipsis] : middle),
     ...(leftEllipsis && rightEllipsis ? middle : []),
