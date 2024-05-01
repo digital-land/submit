b1918587186d9f94f82d77358f442cf85752aff5
George Goodall <ggoodall@scottlogic.com>
2024-05-01 15:54:31 +0100

    add plain mime type

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -85,13 +85,13 @@ export const finishedProcessingStatuses = [
 ]
 
 export const allowedFileTypes = {
-  csv: 'text/csv',
-  xls: 'application/vnd.ms-excel',
-  xlsx: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  json: 'application/json',
-  geojson: 'application/vnd.geo+json',
-  gml: 'application/gml+xml',
-  gpkg: 'application/gpkg',
-  sqlite: 'application/geopackage+sqlite3',
-  zip: 'application/zip'
+  csv: ['text/csv', 'text/plain'],
+  xls: ['application/vnd.ms-excel'],
+  xlsx: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+  json: ['application/json'],
+  geojson: ['application/vnd.geo+json'],
+  gml: ['application/gml+xml'],
+  gpkg: ['application/gpkg'],
+  sqlite: ['application/geopackage+sqlite3'],
+  zip: ['application/zip']
 }
