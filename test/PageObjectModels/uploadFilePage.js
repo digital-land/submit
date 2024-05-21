5ba3cb7de90568e4adb502a06c655beadf5bf80e
Samriti Sadhu <ssadhu@scottlogic.com>
2024-05-21 10:27:35 +0100

    lint fix

--- test/PageObjectModels/uploadFilePage.js
+++ test/PageObjectModels/uploadFilePage.js
@@ -12,17 +12,16 @@ export default class UploadFilePage extends BasePage {
     await this.page.getByText('Upload data').click()
     const fileChooser = await fileChooserPromise
     await fileChooser.setFiles(filePath)
-
   }
 
   async clickContinue (skipVerification) {
     await super.clickContinue()
     await this.page.waitForTimeout(10000)
-    const screenshotPath = 'test/datafiles/screenshot.png';
-    const screenshotBuffer = await this.page.screenshot();
-    fs.writeFileSync(screenshotPath, screenshotBuffer);
-    console.log(`Screenshot saved to: ${screenshotPath}`);
-    console.log("URL here:",this.page.url())
+    const screenshotPath = 'test/datafiles/screenshot.png'
+    const screenshotBuffer = await this.page.screenshot()
+    fs.writeFileSync(screenshotPath, screenshotBuffer)
+    console.log(`Screenshot saved to: ${screenshotPath}`)
+    console.log('URL here:', this.page.url())
     return await super.verifyAndReturnPage(StatusPage, skipVerification)
   }
 }
