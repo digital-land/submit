9962256699d6f98967379a6d33d938ba165df5d2
Samriti Sadhu <ssadhu@scottlogic.com>
2024-05-21 10:27:15 +0100

    Adding screenshot to test

--- test/PageObjectModels/uploadFilePage.js
+++ test/PageObjectModels/uploadFilePage.js
@@ -1,5 +1,6 @@
 import BasePage from './BasePage'
 import StatusPage from './statusPage'
+import fs from 'fs'
 
 export default class UploadFilePage extends BasePage {
   constructor (page) {
@@ -11,10 +12,17 @@ export default class UploadFilePage extends BasePage {
     await this.page.getByText('Upload data').click()
     const fileChooser = await fileChooserPromise
     await fileChooser.setFiles(filePath)
+
   }
 
   async clickContinue (skipVerification) {
     await super.clickContinue()
+    await this.page.waitForTimeout(10000)
+    const screenshotPath = 'test/datafiles/screenshot.png';
+    const screenshotBuffer = await this.page.screenshot();
+    fs.writeFileSync(screenshotPath, screenshotBuffer);
+    console.log(`Screenshot saved to: ${screenshotPath}`);
+    console.log("URL here:",this.page.url())
     return await super.verifyAndReturnPage(StatusPage, skipVerification)
   }
 }
