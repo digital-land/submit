b08cc892cf0c8516409c6f2d8abacda0c6ab653d
George Goodall <ggoodall@scottlogic.com>
2024-05-30 10:40:18 +0100

    started adding test outline

--- test/unit/responseDetails.test.js
+++ test/unit/responseDetails.test.js
@@ -1,5 +1,77 @@
+import test from 'node:test'
 import { pagination } from '../../src/models/responseDetails'
 import { describe, it, expect } from 'vitest'
+
+describe('ResponseDetails', () => {
+  // let responseDetails
+  // const mockResponse = [] // Fill with mock data
+  // const mockPagination = {} // Fill with mock data
+  // const mockColumnFieldLog = [] // Fill with mock data
+
+  // beforeEach(() => {
+  //   // responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+  // })
+
+  describe('getRows', () => {
+    test('returns the rows', () => {
+      // TODO: Write test
+    })
+
+    test('returns an empty array if there are no rows and logs an error', () => {
+
+    })
+  })
+
+  describe('getColumnFieldLog', () => {
+    test('returns the column field log', () => {
+      // TODO: Write test
+    })
+
+    test('returns an empty array if there is no column field log and logs an error', () => {
+
+    })
+  })
+
+  describe('getColumns', () => {
+    test('returns the columns', () => {
+
+    })
+
+    test('returns an empty array if there are no rows', () => {
+
+    })
+  })
+
+  test('getFields', () => {
+    // TODO: Write test
+  })
+
+  test('getFieldMappings', () => {
+    // TODO: Write test
+  })
+
+  describe('getRowsWithVerboseColumns', () => {
+    test('returns the rows with verbose columns', () => {
+
+    })
+
+    test('returns an empty array if there are no rows and logs an error', () => {
+
+    })
+  })
+
+  test('getGeometryKey', () => {
+    // TODO: Write test
+  })
+
+  test('getGeometries', () => {
+    // TODO: Write test
+  })
+
+  test('getPagination', () => {
+    // TODO: Write test
+  })
+})
 
 describe('Pagination', () => {
   const testCases = [
