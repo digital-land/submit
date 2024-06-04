b644fc4396a4d91ba10e81ecf58b0d27a440f872
Samriti Sadhu <ssadhu@scottlogic.com>
2024-06-04 14:44:22 +0100

    Setting up the sentry

--- src/serverSetup/sentry.js
+++ src/serverSetup/sentry.js
@@ -1,15 +1,18 @@
 import * as Sentry from '@sentry/node'
 import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
+
 const setupSentry = (app) => {
+  if (process.env.SENTRY_ENABLED?.toLowerCase() === 'true') {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     integrations: [nodeProfilingIntegration()],
-    tracesSampleRate: 1.0,
-    profilesSampleRate: 1.0
+    tracesSampleRate: parseFloat(process.env.SENTRY_TRACING_SAMPLE_RATE || '0.50'),
+    profilesSampleRate: parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE || '0.01'),
   })
-
+  
   Sentry.setupExpressErrorHandler(app)
+  }
 }
 
 export { setupSentry }
