556cc3488138d9ab25a84a6f63ebdea62ed36049
George Goodall <ggoodall@scottlogic.com>
2024-06-04 16:31:47 +0100

    start working on response details tests

--- test/unit/responseDetails.test.js
+++ test/unit/responseDetails.test.js
@@ -1,76 +1,193 @@
-import test from 'node:test'
-import { pagination } from '../../src/models/responseDetails'
-import { describe, it, expect } from 'vitest'
+import ResponseDetails, { pagination } from '../../src/models/responseDetails'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
+import logger from '../../src/utils/logger.js'
 
 describe('ResponseDetails', () => {
-  // let responseDetails
-  // const mockResponse = [] // Fill with mock data
-  // const mockPagination = {} // Fill with mock data
-  // const mockColumnFieldLog = [] // Fill with mock data
+  const mockResponse = [
+    {
+      issue_logs: [],
+      entry_number: 1,
+      converted_row: {
+        id: '4',
+        wkt: 'POINT (423432.0000000000000000 564564.0000000000000000)',
+        name: 'South Jesmond',
+        Layer: 'Conservation Area',
+        'area(ha)': '35.4',
+        geometry: '',
+        'entry-date': '04/04/2025',
+        'start-date': '04/04/2024',
+        'documentation-url': 'www.example.com'
+      }
+    },
+    {
+      issue_logs: [],
+      entry_number: 2,
+      converted_row: {
+        id: '5',
+        wkt: 'POINT (423432.0000000000000000 564564.0000000000000000)',
+        name: 'North Jesmond',
+        Layer: 'Conservation Area',
+        'area(ha)': '35.4',
+        geometry: '',
+        'entry-date': '04/04/2025',
+        'start-date': '04/04/2024',
+        'documentation-url': 'www.example.com'
+      }
+    }
+  ]
 
-  // beforeEach(() => {
-  //   // responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
-  // })
+  const mockPagination = {
+    totalResults: 2,
+    offset: 0,
+    limit: 50
+  }
+
+  const mockColumnFieldLog = [
+    { column: 'id', field: 'ID' },
+    { column: 'wkt', field: 'WKT' },
+    { column: 'name', field: 'Name' },
+    { column: 'Layer', field: 'Layer' },
+    { column: 'area(ha)', field: 'Area' },
+    { column: 'geometry', field: 'Geometry' },
+    { column: 'entry-date', field: 'Entry Date' },
+    { column: 'start-date', field: 'Start Date' },
+    { column: 'documentation-url', field: 'Documentation URL' }
+  ]
+
+  vi.mock('../utils/logger.js', () => {
+    return {
+      default: {
+        error: vi.fn()
+      }
+    }
+  })
+
+  const loggerErrorSpy = vi.spyOn(logger, 'error')
+
+  beforeEach(() => {
+    loggerErrorSpy.mockClear()
+  })
 
   describe('getRows', () => {
-    test('returns the rows', () => {
-      // TODO: Write test
+    it('returns the rows', () => {
+      const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+      const result = responseDetails.getRows()
+      expect(result).toBe(mockResponse)
     })
 
-    test('returns an empty array if there are no rows and logs an error', () => {
-
+    it('returns an empty array if there are no rows and logs an error', () => {
+      const responseDetails = new ResponseDetails(undefined, undefined, undefined)
+      const result = responseDetails.getRows()
+      expect(result).toStrictEqual([])
+      expect(loggerErrorSpy).toHaveBeenCalled()
     })
   })
 
   describe('getColumnFieldLog', () => {
-    test('returns the column field log', () => {
-      // TODO: Write test
+    it('returns the column field log', () => {
+      const responseDetails = new ResponseDetails(undefined, undefined, mockColumnFieldLog)
+      const result = responseDetails.getColumnFieldLog()
+      expect(result).toStrictEqual(mockColumnFieldLog)
     })
 
-    test('returns an empty array if there is no column field log and logs an error', () => {
+    it('returns an empty array if there is no column field log and logs an error', () => {
+      const responseDetails = new ResponseDetails(undefined, undefined, undefined)
+      const result = responseDetails.getColumnFieldLog()
+      expect(result).toStrictEqual([])
+      expect(loggerErrorSpy).toHaveBeenCalled()
+    })
+  })
 
+  describe('getFields', () => {
+    it('returns the unique fields from the column keys', () => {
+      const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+      const result = responseDetails.getFields()
+      const expected = ['ID', 'WKT', 'Name', 'Layer', 'Area', 'Geometry', 'Entry Date', 'Start Date', 'Documentation URL']
+      expect(result).toEqual(expected)
     })
   })
 
   describe('getColumns', () => {
-    test('returns the columns', () => {
-
+    it('returns the columns', () => {
+      const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+      responseDetails.getFields = vi.fn(() => ['ID', 'WKT', 'Name', 'Layer', 'Area', 'Geometry', 'Entry Date', 'Start Date', 'Documentation URL'])
+      const result = responseDetails.getColumns()
+      expect(result).toEqual(['id', 'wkt', 'name', 'Layer', 'area(ha)', 'geometry', 'entry-date', 'start-date', 'documentation-url'])
     })
 
-    test('returns an empty array if there are no rows', () => {
-
+    it('returns an empty array if there are no rows', () => {
+      const responseDetails = new ResponseDetails(undefined, undefined, undefined)
+      const result = responseDetails.getColumns()
+      expect(result).toStrictEqual([])
     })
   })
 
-  test('getFields', () => {
-    // TODO: Write test
-  })
-
-  test('getFieldMappings', () => {
-    // TODO: Write test
+  it('getFieldMappings', () => {
+    const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+    const result = responseDetails.getFieldMappings()
+    const expected = {
+      ID: 'id',
+      WKT: 'wkt',
+      Name: 'name',
+      Layer: 'Layer',
+      Area: 'area(ha)',
+      Geometry: 'geometry',
+      'Entry Date': 'entry-date',
+      'Start Date': 'start-date',
+      'Documentation URL': 'documentation-url'
+    }
+    expect(result).toEqual(expected)
   })
 
   describe('getRowsWithVerboseColumns', () => {
-    test('returns the rows with verbose columns', () => {
-
+    vi.mock('../utils/getVerboseColumns.js', () => {
+      return vi.fn((row, columnFieldLog) => {
+        return { row, columnFieldLog }
+      })
     })
 
-    test('returns an empty array if there are no rows and logs an error', () => {
-
+    it('returns the rows with verbose columns', () => {
+      const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+      const result = responseDetails.getRowsWithVerboseColumns()
+      const expected = [
+        {
+          entryNumber: 1,
+          hasErrors: false,
+          columns: { row: mockResponse[0], columnFieldLog: mockColumnFieldLog }
+        },
+        {
+          entryNumber: 2,
+          hasErrors: false,
+          columns: { row: mockResponse[1], columnFieldLog: mockColumnFieldLog }
+        }
+      ]
+      expect(result).toStrictEqual(expected)
     })
+
+    // it('returns the rows with verbose columns and filters out non-errors', () => {
+    //   const responseDetails = new ResponseDetails(mockResponse, mockPagination, mockColumnFieldLog)
+    //   const result = responseDetails.getRowsWithVerboseColumns(true)
+    // })
+
+    // it('returns an empty array if there are no rows and logs an error', () => {
+    //   const responseDetails = new ResponseDetails(undefined, undefined, undefined)
+    //   const result = responseDetails.getRowsWithVerboseColumns()
+    //   expect(result).toStrictEqual([])
+    //   expect(loggerErrorSpy).toHaveBeenCalled()
+    // })
   })
 
-  test('getGeometryKey', () => {
-    // TODO: Write test
-  })
+  // it('getGeometryKey', () => {
+  //   // TODO: Write test
+  // })
 
-  test('getGeometries', () => {
-    // TODO: Write test
-  })
+  // it('getGeometries', () => {
+  //   // TODO: Write test
+  // })
 
-  test('getPagination', () => {
-    // TODO: Write test
-  })
+  // it('getPagination', () => {
+  //   // TODO: Write test
+  // })
 })
 
 describe('Pagination', () => {
