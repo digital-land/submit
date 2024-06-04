9449fbceeec81510f67b06c06648ce3bea561860
Samriti Sadhu <ssadhu@scottlogic.com>
2024-06-04 14:47:12 +0100

    Setting the rate as 0.01

--- src/serverSetup/sentry.js
+++ src/serverSetup/sentry.js
@@ -1,17 +1,16 @@
 import * as Sentry from '@sentry/node'
 import { nodeProfilingIntegration } from '@sentry/profiling-node'
 
-
 const setupSentry = (app) => {
   if (process.env.SENTRY_ENABLED?.toLowerCase() === 'true') {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [nodeProfilingIntegration()],
-    tracesSampleRate: parseFloat(process.env.SENTRY_TRACING_SAMPLE_RATE || '0.50'),
-    profilesSampleRate: parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE || '0.01'),
-  })
-  
-  Sentry.setupExpressErrorHandler(app)
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      integrations: [nodeProfilingIntegration()],
+      tracesSampleRate: parseFloat(process.env.SENTRY_TRACING_SAMPLE_RATE || '0.01'),
+      profilesSampleRate: parseFloat(process.env.SENTRY_PROFILES_SAMPLE_RATE || '0.01')
+    })
+
+    Sentry.setupExpressErrorHandler(app)
   }
 }
 
