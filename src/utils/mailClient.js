07ec78efa8d79fa8afa46f63916be37fd8a615fd
George Goodall <ggoodall@scottlogic.com>
2024-06-27 14:44:19 +0100

    add some testing

--- src/utils/mailClient.js
+++ src/utils/mailClient.js
@@ -6,7 +6,7 @@ dotenv.config()
 const notifyClient = new NotifyClient(process.env.GOVUK_NOTIFY_API_KEY)
 
 export const sendEmail = async (emailAddress, templateId, personalisation) => {
-  notifyClient.sendEmail(templateId, emailAddress, { personalisation })
+  return notifyClient.sendEmail(templateId, emailAddress, { personalisation })
     .then(response => console.log(response))
     .catch(err => console.error(err))
 }
