627a00d968200b8999909889a91e72c7cc90d7a8
George Goodall <ggoodall@scottlogic.com>
2024-06-27 13:36:38 +0100

    linting

--- test/unit/checkAnswersController.test.js
+++ test/unit/checkAnswersController.test.js
@@ -7,69 +7,67 @@ import { sendEmail } from '../../src/utils/mailClient.js'
 vi.mock('../../src/utils/mailClient.js')
 
 describe('Check answers controller', () => {
-    let checkAnswersController
-    let sendEmailMock
+  let checkAnswersController
+  let sendEmailMock
 
-    beforeEach(() => {
-        checkAnswersController = new CheckAnswersController({
-            route: '/dataset'
-        })
-
-        sendEmailMock = vi.fn()
-
-        sendEmail.mockImplementation(sendEmailMock)
-
+  beforeEach(() => {
+    checkAnswersController = new CheckAnswersController({
+      route: '/dataset'
     })
 
+    sendEmailMock = vi.fn()
 
-    describe('send emails', () => {
-        it('should get the values from the session model and then send the request and acknowledgement emails', () => {
-            // Mock req, res, next
-            const req = {
-                sessionModel: {
-                    get: vi.fn().mockImplementation((key) => {
-                        const values = {
-                            name: 'John Doe',
-                            email: 'JohnDoe@mail.com',
-                            lpa: 'LPA',
-                            dataset: 'Dataset',
-                            'documentation-url': 'Documentation URL',
-                            'endpoint-url': 'Endpoint URL'
-                        }
-                        return values[key]
-                    })
-                }
+    sendEmail.mockImplementation(sendEmailMock)
+  })
+
+  describe('send emails', () => {
+    it('should get the values from the session model and then send the request and acknowledgement emails', () => {
+      // Mock req, res, next
+      const req = {
+        sessionModel: {
+          get: vi.fn().mockImplementation((key) => {
+            const values = {
+              name: 'John Doe',
+              email: 'JohnDoe@mail.com',
+              lpa: 'LPA',
+              dataset: 'Dataset',
+              'documentation-url': 'Documentation URL',
+              'endpoint-url': 'Endpoint URL'
             }
+            return values[key]
+          })
+        }
+      }
 
-            const res = {}
-            const next = vi.fn()
+      const res = {}
+      const next = vi.fn()
 
-            checkAnswersController.sendEmails(req, res, next)
+      checkAnswersController.sendEmails(req, res, next)
 
-            expect(req.sessionModel.get).toHaveBeenCalledWith('name')
-            expect(req.sessionModel.get).toHaveBeenCalledWith('email')
-            expect(req.sessionModel.get).toHaveBeenCalledWith('lpa')
-            expect(req.sessionModel.get).toHaveBeenCalledWith('dataset')
-            expect(req.sessionModel.get).toHaveBeenCalledWith('documentation-url')
-            expect(req.sessionModel.get).toHaveBeenCalledWith('endpoint-url')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('name')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('email')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('lpa')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('dataset')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('documentation-url')
+      expect(req.sessionModel.get).toHaveBeenCalledWith('endpoint-url')
 
-            expect(sendEmailMock).toHaveBeenCalledWith(config.email.dataManagementEmail, config.email.templates.RequestTemplateId, {
-                name: 'John Doe',
-                email: 'JohnDoe@mail.com',
-                organisation: 'LPA',
-                dataset: 'Dataset',
-                'documentation-url': 'Documentation URL',
-                endpoint: 'Endpoint URL'
-            })
+      expect(sendEmailMock).toHaveBeenCalledWith(config.email.dataManagementEmail, config.email.templates.RequestTemplateId, {
+        name: 'John Doe',
+        email: 'JohnDoe@mail.com',
+        organisation: 'LPA',
+        dataset: 'Dataset',
+        'documentation-url': 'Documentation URL',
+        endpoint: 'Endpoint URL'
+      })
 
-            expect(sendEmailMock).toHaveBeenCalledWith('JohnDoe@mail.com', config.email.templates.AcknowledgementTemplateId, {
-                name: 'John Doe',
-                email: 'JohnDoe@mail.com',
-                organisation: 'LPA',
-                endpoint: 'Endpoint URL',
-                'documentation-url': 'Documentation URL',
-                dataset: 'Dataset'
-            })
-        })
+      expect(sendEmailMock).toHaveBeenCalledWith('JohnDoe@mail.com', config.email.templates.AcknowledgementTemplateId, {
+        name: 'John Doe',
+        email: 'JohnDoe@mail.com',
+        organisation: 'LPA',
+        endpoint: 'Endpoint URL',
+        'documentation-url': 'Documentation URL',
+        dataset: 'Dataset'
+      })
     })
+  })
 })
