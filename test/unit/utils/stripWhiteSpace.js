55f11358ca616fd091a7dd8e8709c7a8beda8ec1
George Goodall <ggoodall@scottlogic.com>
2024-07-02 15:43:48 +0100

    linting

--- test/unit/utils/stripWhiteSpace.js
+++ test/unit/utils/stripWhiteSpace.js
@@ -1,5 +1,4 @@
 // this function strips out unnecessary whitespace from our html to make sure comparisons are accurate
 export const stripWhitespace = (str) => {
-    return str.replace(/(\r\n|\n|\r)/gm, '').replace(/\t/gm, '').replace(/\s+/g, ' ').replace(/>\s+</g, '><').replace(/>\s+/g, '>').replace(/\s+</g, '<').replace(/\s+>/g, '>').replace(/<\s+/g, '<')
-  }
-  
+  return str.replace(/(\r\n|\n|\r)/gm, '').replace(/\t/gm, '').replace(/\s+/g, ' ').replace(/>\s+</g, '><').replace(/>\s+/g, '>').replace(/\s+</g, '<').replace(/\s+>/g, '>').replace(/<\s+/g, '<')
+}
