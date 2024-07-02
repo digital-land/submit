c83a4ef3bd35a673ed4370d965d6df14ec0f435b
George Goodall <ggoodall@scottlogic.com>
2024-07-02 16:04:52 +0100

    finish off the last fest tests and change file from .html to .js

--- test/unit/choose-datasetPage.test.js
+++ test/unit/choose-datasetPage.test.js
@@ -1,7 +1,8 @@
-import { describe } from 'vitest'
+import { describe, it } from 'vitest'
 import { setupNunjucks } from '../../src/serverSetup/nunjucks.js'
 import { runGenericPageTests } from './generic-page.js'
 import config from '../../config/index.js'
+import { testValidationErrorMessage } from './validation-tests.js'
 
 const nunjucks = setupNunjucks()
 
@@ -14,5 +15,19 @@ describe('choose dataset View', () => {
   runGenericPageTests(html, {
     pageTitle: `Choose dataset - ${config.serviceName}`,
     serviceName: config.serviceName
+  })
+
+  it('should display an error message when the dataset field is empty', () => {
+    const params = {
+      errors: {
+        dataset: {
+          type: 'required'
+        }
+      }
+    }
+
+    const html = nunjucks.render('choose-dataset.html', params)
+
+    testValidationErrorMessage(html, 'dataset', 'Select a dataset')
   })
 })
