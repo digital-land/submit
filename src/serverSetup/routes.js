3126db8da5f55c48e7a54d3b38bb139bf8b4a941
George Goodall <ggoodall@scottlogic.com>
2024-07-08 11:56:15 +0100

    split up form wizards

--- src/serverSetup/routes.js
+++ src/serverSetup/routes.js
@@ -1,14 +1,12 @@
-import formWizard from '../routes/form-wizard/index.js'
+import checkFormWizard from '../routes/form-wizard/check/index.js'
+import endpointSubmissionFormFormWisard from '../routes/form-wizard/endpoint-submission-form/index.js'
 import accessibility from '../routes/accessibility.js'
 import polling from '../routes/api.js'
 import health from '../routes/health.js'
 
 export function setupRoutes (app) {
-<<<<<<< HEAD Need to setup two form wizard instances here to handle the two different form wizards
-  app.use('/', formWizard)
-=======
-    app.use('/', formWizard)
->>>>>>> endpoint-submission-form/main
+  app.use('/', checkFormWizard)
+  app.use('/submit', endpointSubmissionFormFormWisard)
   app.use('/accessibility', accessibility)
   app.use('/api', polling)
   app.use('/health', health)
