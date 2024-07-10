afdd200e84f721c159cc6b32236bcb7652f8d848
Roland Sadowski <szabla@gmail.com>
2024-07-10 12:08:19 +0100

    form submission: error handling and logging for email notifications

--- test/unit/checkAnswersController.test.js
+++ test/unit/checkAnswersController.test.js
@@ -6,7 +6,41 @@ import config from '../../config/index.js'
 
 vi.mock('../../src/utils/mailClient.js')
 
-describe('Check answers controller', () => {
+function makeRequest () {
+  return {
+    sessionModel: {
+      get: vi.fn().mockImplementation((key) => {
+        const values = {
+          name: 'John Doe',
+          email: 'JohnDoe@mail.com',
+          lpa: 'LPA',
+          dataset: 'Dataset',
+          'documentation-url': 'Documentation URL',
+          'endpoint-url': 'Endpoint URL'
+        }
+        return values[key]
+      })
+    }
+  }
+}
+
+describe('Handle email notification handlers', async () => {
+  const CheckAnswersController = await vi.importActual('../../src/controllers/CheckAnswersController.js')
+  const sendEmailMock = vi.fn(() => Promise.reject(new Error('something went wrong')))
+  notifyClient.sendEmail = sendEmailMock
+
+  const controller = new CheckAnswersController.default({ route: '/dataset' })
+  const req = makeRequest()
+  const res = {}
+  const next = vi.fn()
+
+  it('should reuturn list of errors when failed to send email', async () => {
+    const result = await controller.sendEmails(req, res, next)
+    expect(result.errors.length).toBe(2)
+  })
+})
+
+describe('Check answers controller', async () => {
   let CheckAnswersController
   let checkAnswersController
   let sendEmailMock
@@ -22,57 +56,33 @@ describe('Check answers controller', () => {
   })
 
   describe('send emails', () => {
-    it('should get the values from the session model and then send the request and acknowledgement emails', () => {
+    it('should get the values from the session model and then send the request and acknowledgement emails', async () => {
       // Mock req, res, next
-      const req = {
-        sessionModel: {
-          get: vi.fn().mockImplementation((key) => {
-            const values = {
-              name: 'John Doe',
-              email: 'JohnDoe@mail.com',
-              lpa: 'LPA',
-              dataset: 'Dataset',
-              'documentation-url': 'Documentation URL',
-              'endpoint-url': 'Endpoint URL'
-            }
-            return values[key]
-          })
-        }
-      }
-
+      const req = makeRequest()
       const res = {}
       const next = vi.fn()
 
-      checkAnswersController.sendEmails(req, res, next)
+      await checkAnswersController.sendEmails(req, res, next)
+
+      const personalisation = {
+        name: 'John Doe',
+        email: 'JohnDoe@mail.com',
+        organisation: 'LPA',
+        dataset: 'Dataset',
+        'documentation-url': 'Documentation URL',
+        endpoint: 'Endpoint URL'
+      }
 
       expect(sendEmailMock).toHaveBeenCalledWith(
         config.email.templates.RequestTemplateId,
         config.email.dataManagementEmail,
-        {
-          personalisation: {
-            name: 'John Doe',
-            email: 'JohnDoe@mail.com',
-            organisation: 'LPA',
-            dataset: 'Dataset',
-            'documentation-url': 'Documentation URL',
-            endpoint: 'Endpoint URL'
-          }
-        }
+        { personalisation }
       )
 
       expect(sendEmailMock).toHaveBeenCalledWith(
         config.email.templates.AcknowledgementTemplateId,
         'JohnDoe@mail.com',
-        {
-          personalisation: {
-            name: 'John Doe',
-            email: 'JohnDoe@mail.com',
-            organisation: 'LPA',
-            endpoint: 'Endpoint URL',
-            'documentation-url': 'Documentation URL',
-            dataset: 'Dataset'
-          }
-        }
+        { personalisation }
       )
     })
   })
