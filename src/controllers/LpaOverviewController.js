0a489c5a0cde89ca0a58f779970275495c0253c6
George Goodall <ggoodall@scottlogic.com>
2024-07-11 15:09:36 +0100

    now get mock data from the mock api, and use it to populate the template

--- src/controllers/LpaOverviewController.js
+++ src/controllers/LpaOverviewController.js
@@ -1,11 +1,33 @@
-import performanceDbApi from '../services/performanceDbApi' // Assume you have an API service module
+import performanceDbApi from '../services/performanceDbApi.js' // Assume you have an API service module
 
-class LpaOverviewController {
+const LpaOverviewController = {
   async getOverview (req, res, next) {
     try {
-      const response = await performanceDbApi.getLpaOverview() // Make API request
+      const lpa = req.params.lpa
+
+      const response = await performanceDbApi.getLpaOverview(lpa) // Make API request
       const data = response.data
-      res.render('manage/lpa-overview.html', { data })
+
+      const datasets = Object.entries(data.datasets).map(([key, value]) => {
+        return { ...value, slug: key }
+      })
+      const totalDatasets = datasets.length
+      const datasetsWithEndpoints = datasets.filter(item => item.endpoint !== null).length
+      const datasetsWithIssues = datasets.filter(item => item.issue).length
+      const datasetsWithErrors = datasets.filter(item => item.error).length
+
+      const params = {
+        organisation: {
+          name: data.name
+        },
+        datasets,
+        totalDatasets,
+        datasetsWithEndpoints,
+        datasetsWithIssues,
+        datasetsWithErrors
+      }
+
+      res.render('manage/lpa-overview.html', params)
     } catch (error) {
       next(error)
     }
--- src/routes/manage.js
+++ src/routes/manage.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import LpaOverviewController from '../controllers/LpaOverviewController'
+import LpaOverviewController from '../controllers/LpaOverviewController.js'
 
 const router = express.Router()
 
-router.get('/lpa-overview', LpaOverviewController.getOverview)
+router.get('/:lpa/overview', LpaOverviewController.getOverview)
 
 export default router
