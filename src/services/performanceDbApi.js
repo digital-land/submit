7609f6e1a174b6fbcccd7c5389f864597eedf577
George Goodall <ggoodall@scottlogic.com>
2024-07-12 16:27:57 +0100

    remove old method

--- src/services/performanceDbApi.js
+++ src/services/performanceDbApi.js
@@ -5,50 +5,6 @@ import datasette from './datasette.js'
 */
 
 export default {
-  getLpaOverviewOld: async (lpa) => {
-    return {
-      name: 'Borechester City Council',
-      datasets: {
-        'article-4-direction': {
-          endpoint: null
-        },
-        'article-4-direction-area': {
-          endpoint: null
-        },
-        'conservation-area': {
-          endpoint: 'http://conservation-area.json',
-          error: null,
-          issue: 'Endpoint has not been updated since 21 May 2023'
-        },
-        'conservation-area-document': {
-          endpoint: 'http://conservation-area-document.json',
-          error: null,
-          issue: null
-        },
-        'listed-building-outline': {
-          endpoint: 'http://listed-building-outline.json',
-          error: null,
-          issue: null
-        },
-        tree: {
-          endpoint: 'http://tree.json',
-          error: null,
-          issue: 'There are 20 issues in this dataset'
-        },
-        'tree-preservation-order': {
-          endpoint: 'http://tree-preservation-order.json',
-          error: 'Error connecting to endpoint',
-          issue: null
-        },
-        'tree-preservation-zone': {
-          endpoint: 'http://tree-preservation-zone.json',
-          error: 'Error connecting to endpoint',
-          issue: null
-        }
-      }
-    }
-  },
-
   getLpaOverview: async (lpa) => {
     const query = `
     SELECT
