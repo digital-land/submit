85100c1347afb290b76ed34b0d2537736bef4e6d
George Goodall <ggoodall@scottlogic.com>
2024-07-12 15:40:01 +0100

    updated with samritis new query: now showing issues

--- src/services/performanceDbApi.js
+++ src/services/performanceDbApi.js
@@ -50,37 +50,88 @@ export default {
   },
 
   getLpaOverview: async (lpa) => {
-    const query =
-    `SELECT
-        p.organisation,
-        o.name,
-        p.dataset,
-        rle.endpoint,
-        rle.status,
-        rle.exception
-    FROM
-        provision p
-    INNER JOIN
-        organisation o ON o.organisation = p.organisation
-    LEFT JOIN
-        reporting_latest_endpoints rle
-        ON REPLACE(rle.organisation, '-eng', '') = p.organisation
-        AND rle.pipeline = p.dataset
-    WHERE p.organisation = '${lpa}'
-    ORDER BY
-        p.organisation,
-        o.name`
+    const query = `
+    SELECT
+    p.organisation,
+    o.name,
+    p.dataset,
+    rle.pipeline,
+    rle.endpoint,
+    rle.resource,
+    rle.exception,
+    rle.status as http_status,
+    case 
+           when (rle.status != '200') then 'Error'
+           when (it.severity = 'error') then 'Issue'
+           when (it.severity = 'warning') then 'Warning'
+           else 'No issues'
+           end as status,
+    case
+            when (it.severity = 'info') then ''
+            else i.issue_type
+        end as issue_type,
+        case
+            when (it.severity = 'info') then ''
+            else it.severity
+        end as severity,
+        it.responsibility,
+        COUNT(
+            case
+            when it.severity != 'info' then 1
+            else null
+            end
+        ) as issue_count
+FROM
+    provision p
+LEFT JOIN
+    organisation o ON o.organisation = p.organisation
+LEFT JOIN
+    reporting_latest_endpoints rle
+    ON REPLACE(rle.organisation, '-eng', '') = p.organisation
+    AND rle.pipeline = p.dataset
+LEFT JOIN
+    issue i ON rle.resource = i.resource AND rle.pipeline = i.dataset
+LEFT JOIN
+    issue_type it ON i.issue_type = it.issue_type AND it.severity != 'info'
+WHERE
+    p.organisation = '${lpa}'
+GROUP BY
+    p.organisation,
+    p.dataset,
+    o.name,
+    rle.pipeline,
+    rle.endpoint
+ORDER BY
+    p.organisation,
+    o.name;
+`
 
     const result = await datasette.runQuery(query)
 
-    const datasets = result.rows.reduce((accumulator, row) => {
+    // convert the rows into an easier to access format
+    const columns = result.columns
+    const rows = result.rows.map((row) => {
+      return row.reduce((acc, val, index) => {
+        acc[columns[index]] = val;
+        return acc;
+      }, {});
+    })
+
+    const datasets = rows.reduce((accumulator, row) => {
       let error
-      if (row[4] !== 200 || row[5] !== '') {
-        error = row[5] !== '' ? row[5] : `endpoint returned with a status of ${row[4]}`
+      if (row.http_status !== '200' || row.exception !== '') {
+        error = row.exception !== '' ? row.exception : `endpoint returned with a status of ${row.http_status}`
       }
-      accumulator[row[2]] = {
-        endpoint: row[3],
-        error
+
+      let issue
+      if (row.issue_count > 0){
+        issue = `There are ${row.issue_count} issues in this dataset`
+      }
+
+      accumulator[row.dataset] = {
+        endpoint: row.endpoint,
+        error,
+        issue
       }
       return accumulator
     }, {})
