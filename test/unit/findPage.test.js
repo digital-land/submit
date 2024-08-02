50a243f56c81834f319d574b7076e596084b86d0
George Goodall <ggoodall@scottlogic.com>
2024-08-02 14:22:31 +0100

    finish of tests for find.html

--- test/unit/findPage.test.js
+++ test/unit/findPage.test.js
@@ -42,9 +42,9 @@ describe('Organisations Find Page', () => {
         {
           name: 'Brighton'
         }
-      ],
+      ]
     },
-    serviceName: "mock service name"
+    serviceName: 'mock service name'
   }
 
   const html = nunjucks.render('organisations/find.html', params)
@@ -54,34 +54,36 @@ describe('Organisations Find Page', () => {
 
   runGenericPageTests(html, {
     pageTitle: 'Find your organisation - mock service name',
-    serviceName: "mock service name"
+    serviceName: 'mock service name'
   })
 
   it('correct has a form element with the correct data-filter attribute', () => {
     const formElement = document.querySelector('form')
-    expect(formElement.getAttribute('data-filter')).toBe('form')  
+    expect(formElement.getAttribute('data-filter')).toBe('form')
   })
 
   it('correctly has elements with the data-filter=block and data-filter=inner block attributes', () => {
-    const blockElements = document.querySelectorAll('[data-filter="block"]');
-    expect(blockElements.length).toBeGreaterThan(0);
-  
-    const innerBlockElements = document.querySelectorAll('[data-filter="inner-block"]');
-    expect(innerBlockElements.length).toBeGreaterThan(0);
+    const blockElements = document.querySelectorAll('[data-filter="block"]')
+    expect(blockElements.length).toBeGreaterThan(0)
+
+    const innerBlockElements = document.querySelectorAll('[data-filter="inner-block"]')
+    expect(innerBlockElements.length).toBeGreaterThan(0)
 
     expect(blockElements.length).toEqual(innerBlockElements.length)
   })
 
   it('Renders the correct organisation list with appropriate attributes', () => {
-    const organisationList = document.querySelector('.')
+    const organisationList = document.querySelector('#search_results')
     expect(organisationList.children.length).toBe(Object.keys(params.alphabetisedOrgs).length)
 
     Object.keys(params.alphabetisedOrgs).forEach((letter, i) => {
       const organisationSection = organisationList.children[i]
-      expect(organisationSection.querySelector('.govuk-accordion__header').textContent).toBe(letter)
+      expect(organisationSection.querySelector('.blockHeading').textContent).toBe(letter)
       const organisationListItems = organisationSection.querySelector('.govuk-list').children
       params.alphabetisedOrgs[letter].forEach((organisation, j) => {
         expect(organisationListItems[j].textContent).toContain(organisation.name)
+        expect(organisationListItems[j].getAttribute('data-filter')).toEqual('item')
+        expect(organisationListItems[j].getAttribute('data-filter-terms')).toEqual(organisation.name)
       })
     })
   })
