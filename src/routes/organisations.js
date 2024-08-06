5449dbc058927f07eb0fbe2985f2f380366cecc6
George Goodall <ggoodall@scottlogic.com>
2024-08-06 15:42:58 +0100

    linting

--- src/routes/organisations.js
+++ src/routes/organisations.js
@@ -7,10 +7,8 @@ router.get('/', OrganisationsController.getOrganisations)
 
 router.get('/:lpa/overview', OrganisationsController.getOverview)
 
-
 router.get('/:lpa/dataset/:dataset', OrganisationsController.getDatasetTaskList)
 
 router.get('/:lpa/dataset/:dataset/get-started', OrganisationsController.getGetStarted)
-
 
 export default router
