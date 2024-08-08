1a0f85e7837d3c95910746c3f602e2e795c0f1dc
George Goodall <ggoodall@scottlogic.com>
2024-08-08 09:41:42 +0100

    add in new datasets

--- src/controllers/datasetController.js
+++ src/controllers/datasetController.js
@@ -5,13 +5,36 @@ import PageController from './pageController.js'
 // ToDo: we shouldn't hardcode these values here, should we get them from the API
 //  maybe take from specification
 import { dataSubjects } from '../utils/utils.js'
+import datasette from '../services/datasette.js'
+import logger from '../utils/logger.js'
 
 class DatasetController extends PageController {
-  locals (req, res, next) {
-    const availableDataSubjects = Object.values(dataSubjects).filter(dataSubject => dataSubject.available)
-    const dataSets = Object.values(availableDataSubjects).map(dataSubject => dataSubject.dataSets).flat()
-    const availableDatasets = dataSets.filter(dataSet => dataSet.available)
-    availableDatasets.sort((a, b) => a.text.localeCompare(b.text))
+  async locals (req, res, next) {
+    // const availableDataSubjects = Object.values(dataSubjects).filter(dataSubject => dataSubject.available)
+    // const dataSets = Object.values(availableDataSubjects).map(dataSubject => dataSubject.dataSets).flat()
+    // const availableDatasets = dataSets.filter(dataSet => dataSet.available)
+    // availableDatasets.sort((a, b) => a.text.localeCompare(b.text))
+
+
+    const datasets = await datasette.runQuery(`select DISTINCT p.dataset, d.name
+                        from provision p
+                        LEFT JOIN dataset d ON p.dataset = d.dataset`)
+
+    const availableDatasets = datasets.formattedData.map((dataset) => {
+      const value = dataset.dataset
+
+      let text
+
+      if(dataset.name && dataset.name.trim()){
+        text = dataset.name
+      }else{
+        text = dataset.dataset
+        logger.warn(`Missing name value for the dataset ${dataset.dataset}`)
+      }
+
+      return { text, value }
+    })
+
 
     req.form.options.datasetItems = availableDatasets
 
@@ -19,17 +42,14 @@ class DatasetController extends PageController {
   }
 
   // we shouldn't need this here but as we dont currently set the datasubject, we need to do so here
-  post (req, res, next) {
+  async post (req, res, next) {
     const dataset = req.body.dataset
-    // set the data-subject based on the dataset selected
-    let dataSubject = ''
-    for (const [key, value] of Object.entries(dataSubjects)) {
-      if (value.dataSets.find(dataSet => dataSet.value === dataset)) {
-        dataSubject = key
-        break
-      }
-    }
-    req.body['data-subject'] = dataSubject
+
+    const result = await datasette.runQuery(`select collection FROM dataset WHERE dataset = '${dataset}'`)
+
+    req.body['data-subject'] = result.formattedData[0].collection
+
+
     super.post(req, res, next)
   }
 
@@ -41,6 +61,9 @@ class DatasetController extends PageController {
     }
 
     const dataSubject = Object.values(dataSubjects).find(dataSubject => dataSubject.dataSets.find(dataSet => dataSet.value === dataset))
+    if(!dataSubject){
+      return false
+    }
     const dataSet = dataSubject.dataSets.find(dataSet => dataSet.value === dataset)
     return dataSet.requiresGeometryTypeSelection || false
   }
