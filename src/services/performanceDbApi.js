813034f3a6544bb1ada7faf56394cb597d9a54bb
Roland Sadowski <szabla@gmail.com>
2024-08-14 08:59:32 +0100

    ensure issue_count is passed to the templates

--- src/services/performanceDbApi.js
+++ src/services/performanceDbApi.js
@@ -137,7 +137,8 @@ ORDER BY
     const datasets = result.formattedData.reduce((accumulator, row) => {
       accumulator[row.dataset] = {
         endpoint: row.endpoint,
-        status: row.status
+        status: row.status,
+        issue_count: row.issue_count
       }
       return accumulator
     }, {})
