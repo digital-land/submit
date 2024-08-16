c52443ab41553e9b63bfb3b68c45550cd2e8ae5a
George Goodall <ggoodall@scottlogic.com>
2024-08-16 12:43:57 +0100

    update check links

--- src/serverSetup/routes.js
+++ src/serverSetup/routes.js
@@ -9,13 +9,12 @@ import privacy from '../routes/privacy.js'
 import cookies from '../routes/cookies.js'
 
 export function setupRoutes (app) {
-  app.use('/', checkFormWizard)
+  app.use('/', manage)
+  app.use('/check', checkFormWizard)
   app.use('/submit', endpointSubmissionFormFormWisard)
   app.use('/organisations', organisations)
 
   app.use('/api', polling)
-
-  app.use('/manage', manage)
 
   app.use('/accessibility', accessibility)
   app.use('/privacy-notice', privacy)
