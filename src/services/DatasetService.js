d75ea8741ef34cc2b82030f4aa77d1ce4d2e963e
George Goodall <ggoodall@scottlogic.com>
2024-09-17 14:21:14 +0100

    add method to get specifications

--- src/services/DatasetService.js
+++ src/services/DatasetService.js
@@ -1,6 +1,8 @@
 import datasette from './datasette.js'
 import logger from '../utils/logger.js'
 import performanceDbApi from './performanceDbApi.js'
+import Papa from 'papaparse'
+import JSON5 from 'json5' // need to use this as spec is written with single quotes. need to pass this onto data designers team to fix
 
 export async function getGeometryEntriesForResourceId (dataset, resourceId) {
   const sql = `
@@ -33,6 +35,20 @@ export async function getLatestDatasetGeometryEntriesForLpa (dataset, lpa) {
   }
 }
 
+/*
+  Needs to get:
+    - Number of records
+    - Number of fields supplied
+    - Number of fields matched
+    - Licence
+    - source documentation url
+    - for each data url
+      - the endpoint
+      - last accessed
+      - last updated
+      - any access errors
+
+*/
 async function getDatasetStatsForResourceId (dataset, resourceId) {
   const sql = `
     SELECT 'numberOfRecords' AS metric, COUNT(*) AS value
@@ -64,10 +80,24 @@ async function getDatasetStatsForResourceId (dataset, resourceId) {
   return formattedData
 }
 
+// async function getColumnSummary (dataset, lpa) {
+//   const sql = `select * from column_field_summary
+//     where resource != ''
+//     and pipeline = '${dataset}'
+//     AND organisation = '${lpa}'
+//     limit 1000`
+
+//   const { formattedData } = await datasette.runQuery(sql, 'performance')
+
+//   return formattedData
+// }
+
 export async function getDatasetStats (dataset, lpa) {
   try {
     const stats = {}
     const { resource: resourceId } = await performanceDbApi.getLatestResource(lpa, dataset)
+    // const columnSummary = await getColumnSummary(dataset, lpa)
+
     const metrics = await getDatasetStatsForResourceId(dataset, resourceId)
 
     metrics.forEach(({ metric, value }) => {
@@ -85,5 +115,36 @@ export async function getDatasetStats (dataset, lpa) {
     )
 
     return {}
+  }
+}
+
+export async function getSpecifications () {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/digital-land/specification/main/specification/specification.csv')
+
+    const csvData = await response.text()
+
+    const initalParse = Papa.parse(csvData, { header: true })
+
+    const fullyParsed = initalParse.data.filter(dataset => dataset.datasets !== '').map((dataset) => {
+      const { json, ...rest } = dataset
+      const formattedJson = JSON5.parse(json)
+      return {
+        ...rest,
+        json: formattedJson
+      }
+    })
+
+    const specifications = fullyParsed.reduce((accumulator, current) => {
+      const datasets = current.datasets.split(';')
+      datasets.forEach((dataset, index) => {
+        accumulator[dataset] = current.json[index]
+      })
+      return accumulator
+    }, {})
+
+    return specifications
+  } catch (error) {
+    console.error(error)
   }
 }
