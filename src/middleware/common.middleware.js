67ea2648c69f6222b56ba61c9ac3b6192aa0cdba
George Goodall <ggoodall@scottlogic.com>
2024-09-23 11:39:46 +0100

    started pulling out middlewares into own files

---
+++ src/middleware/common.middleware.js
@@ -0,0 +1,58 @@
+import logger from '../utils/logger.js'
+import { types } from '../utils/logging.js'
+import performanceDbApi from '../services/performanceDbApi.js'
+import { fetchOne } from './middleware.builders.js'
+
+/**
+ * Middleware. Set `req.handlerName` to a string that will identify
+ * the function that threw the error.
+ *
+ * @param {Error} err
+ * @param {{handlerName: string}} req
+ * @param {*} res
+ * @param {*} next
+ */
+export const logPageError = (err, req, res, next) => {
+  console.assert(req.handlerName, 'handlerName missing ')
+  logger.warn({
+    message: `OrganisationsController.${req.handlerName}(): ${err.message}`,
+    endpoint: req.originalUrl,
+    errorStack: err.stack,
+    errorMessage: err.message,
+    type: types.App
+  })
+  next(err)
+}
+
+export const fetchDatasetInfo = fetchOne({
+  query: ({ params }) => {
+    return `SELECT name, dataset FROM dataset WHERE dataset = '${params.dataset}'`
+  },
+  result: 'dataset'
+})
+
+export const isResourceIdInParams = ({ params }) => !('resourceId' in params)
+
+/**
+ * Was the resource accessed successfully via HTTP?
+ *
+ * @param {*} req
+ * @returns {boolean}
+ */
+export const isResourceAccessible = (req) => req.resourceStatus.status === '200'
+export const isResourceNotAccessible = (req) => !isResourceAccessible(req)
+
+/**
+ * Middleware. Updates req with `resource`.
+ */
+export const fetchLatestResource = fetchOne({
+  query: ({ params }) => performanceDbApi.latestResourceQuery(params.lpa, params.dataset),
+  result: 'resource'
+})
+
+const fetchEntityCount = fetchOne({
+  query: ({ req }) => performanceDbApi.entityCountQuery(req.resource.resource),
+  result: 'entityCount',
+  dataset: FetchOptions.fromParams,
+  fallbackPolicy: FetchOneFallbackPolicy.continue
+})
