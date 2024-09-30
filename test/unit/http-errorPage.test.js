33de3396e7d846735844c35ae084c352bfc5679d
George Goodall <ggoodall@scottlogic.com>
2024-09-30 10:48:05 +0100

    implemented mocker code

--- test/unit/http-errorPage.test.js
+++ test/unit/http-errorPage.test.js
@@ -2,33 +2,22 @@ import { describe, it, expect } from 'vitest'
 import { setupNunjucks } from '../../src/serverSetup/nunjucks.js'
 import { JSDOM } from 'jsdom'
 import { runGenericPageTests } from './generic-page.js'
+import mock from '../utils/mocker.js'
+import { OrgEndpointError } from '../../src/routes/schemas.js'
 
 const nunjucks = setupNunjucks({ datasetNameMapping: new Map() })
 
+const dateRegex = /\d{1,2} \w{3,9} \d{4} at \d{1,2}(?::\d{2})?(?:am|pm)/g
+
 describe('http-error.html', () => {
-  const params = {
-    organisation: {
-      name: 'mock org',
-      organisation: 'mock-org'
-    },
-    dataset: {
-      name: 'mock-dataset',
-      dataset: 'mock-dataset'
-    },
-    errorData: {
-      endpoint_url: 'https://example.com/data-url',
-      http_status: 404,
-      latest_log_entry_date: '2022-01-01T12:00:00Z',
-      latest_200_date: '2022-01-02T12:00:00Z'
-    }
-  }
+  const params = mock(OrgEndpointError)
 
   const html = nunjucks.render('organisations/http-error.html', params)
   const dom = new JSDOM(html)
   const document = dom.window.document
 
   runGenericPageTests(html, {
-    pageTitle: 'mock org - mock-dataset - Task list - Submit and update your planning data'
+    pageTitle: `${params.organisation.name} - ${params.dataset.name} - Task list - Submit and update your planning data`
   })
 
   it('Renders the correct heading', () => {
@@ -48,14 +37,14 @@ describe('http-error.html', () => {
     expect(rows[1].querySelector('.govuk-summary-list__value').textContent).toContain(String(params.errorData.http_status))
 
     expect(rows[2].querySelector('.govuk-summary-list__key').textContent).toContain('Last attempted access')
-    expect(rows[2].querySelector('.govuk-summary-list__value').textContent).toMatch(/\d{1,2} [A-Za-z]{3,9} \d{4} at \d{1,2}(am|pm)/)
+    expect(rows[2].querySelector('.govuk-summary-list__value').textContent).toMatch(dateRegex)
 
     expect(rows[3].querySelector('.govuk-summary-list__key').textContent).toContain('Last successful access')
-    expect(rows[3].querySelector('.govuk-summary-list__value').textContent).toMatch(/\d{1,2} [A-Za-z]{3,9} \d{4} at \d{1,2}(am|pm)/)
+    expect(rows[3].querySelector('.govuk-summary-list__value').textContent).toMatch(dateRegex)
   })
 
   it('re-submit link points to get-started page', () => {
     const resubmitLink = document.querySelector('a.resubmit-link')
-    expect(resubmitLink.getAttribute('href')).toBe(`/organisations/${params.organisation.organisation}/${params.dataset.name}/get-started`)
+    expect(resubmitLink.getAttribute('href')).toBe(`/organisations/${encodeURIComponent(params.organisation.organisation)}/${encodeURIComponent(params.dataset.name)}/get-started`)
   })
 })
