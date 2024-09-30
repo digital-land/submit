2b379ee7296628444e30929d10fbc7bfa2168072
Roland Sadowski <szabla@gmail.com>
2024-09-30 11:52:29 +0100

    remove statrt.html references

---
+++ test/unit/landingPage.test.js
@@ -0,0 +1,16 @@
+// ToDo: need to duplicate this test for submit start page
+
+import { describe } from 'vitest'
+import { setupNunjucks } from '../../src/serverSetup/nunjucks.js'
+import { runGenericPageTests } from './generic-page.js'
+
+const nunjucks = setupNunjucks({ datasetNameMapping: new Map() })
+
+describe('Start View', () => {
+  const params = {}
+  const html = nunjucks.render('landing.html', params)
+
+  runGenericPageTests(html, {
+    // we skip pageTitle since this is the main page, and service name alone is sufficient
+  })
+})
