33fd9d48497ee00f36f241a2eefcb01fbef133f8
George Goodall <ggoodall@scottlogic.com>
2024-09-30 11:32:28 +0100

    mock test data in get-startedPage.test.js

--- test/unit/get-startedPage.test.js
+++ test/unit/get-startedPage.test.js
@@ -5,6 +5,8 @@ import nunjucks from 'nunjucks'
 import addFilters from '../../src/filters/filters'
 import { runGenericPageTests } from './generic-page.js'
 import jsdom from 'jsdom'
+import mocker from '../utils/mocker.js'
+import { OrgGetStarted } from '../../src/routes/schemas.js'
 
 const nunjucksEnv = nunjucks.configure([
   'src/views',
@@ -27,27 +29,20 @@ const datasetNameMapping = new Map([
 addFilters(nunjucksEnv, { datasetNameMapping })
 
 describe('Get Started Page', () => {
-  const params = {
-    organisation: {
-      name: 'Mock org'
-    },
-    dataset: {
-      name: 'World heritage site buffer zone'
-    }
-  }
+  const params = mocker(OrgGetStarted)
   const html = nunjucks.render('organisations/get-started.html', params)
 
   const dom = new jsdom.JSDOM(html)
   const document = dom.window.document
 
   runGenericPageTests(html, {
-    pageTitle: 'Mock org - World heritage site buffer zone - Get started - Submit and update your planning data'
+    pageTitle: `${params.organisation.name} - ${params.dataset.name} - Get started - Submit and update your planning data`
   })
 
   it('Renders the correct headings', () => {
-    expect(document.querySelector('span.govuk-caption-xl').textContent).toEqual('Mock org')
-    expect(document.querySelector('h1').textContent).toContain('World heritage site buffer zone')
-    expect(document.querySelector('h2').textContent).toContain('How to prepare and submit your World heritage site buffer zone data')
+    expect(document.querySelector('span.govuk-caption-xl').textContent).toEqual(params.organisation.name)
+    expect(document.querySelector('h1').textContent).toContain(`${params.dataset.name}`)
+    expect(document.querySelector('h2').textContent).toContain(`How to prepare and submit your ${params.dataset.name} data`)
   })
 
   it('Renders breadcrumbs correctly', () => {
@@ -55,7 +50,7 @@ describe('Get Started Page', () => {
     expect(breadcrumbs.length).toEqual(4)
     expect(breadcrumbs[0].textContent).toContain('Home')
     expect(breadcrumbs[1].textContent).toContain('Organisations')
-    expect(breadcrumbs[2].textContent).toContain('Mock org')
+    expect(breadcrumbs[2].textContent).toContain(params.organisation.name)
     expect(breadcrumbs[3].textContent).toContain('Get started')
   })
 })
