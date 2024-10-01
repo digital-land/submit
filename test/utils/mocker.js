509dd10aba7b6e7605612d090ca487d5f59cbb5b
George Goodall <ggoodall@scottlogic.com>
2024-10-01 10:53:30 +0100

    improve mocker so it can take a seed

--- test/utils/mocker.js
+++ test/utils/mocker.js
@@ -2,7 +2,7 @@ import { toJSONSchema } from '@gcornut/valibot-json-schema'
 import { JSONSchemaFaker } from 'json-schema-faker'
 import { date, number, string } from 'valibot'
 
-export default (schema) => {
+export default (schema, seed) => {
   const jsonSchema = toJSONSchema({
     schema,
     ignoreUnknownValidation: true,
@@ -15,5 +15,44 @@ export default (schema) => {
     dateStrategy: 'string'
   })
 
-  return JSONSchemaFaker.generate(jsonSchema)
+  resetRandomNumberGenerator()
+
+  JSONSchemaFaker.option({
+    random: () => {
+      return generateNextRandomNumber(seed)
+    }
+  })
+
+  const data = JSONSchemaFaker.generate(jsonSchema)
+
+  return data
+}
+
+let xorShiftSeed
+
+const values = []
+
+export const generateNextRandomNumber = (seed) => {
+  try {
+    if (!xorShiftSeed) {
+      if (seed) {
+        xorShiftSeed = seed
+      } else {
+        xorShiftSeed = new Date().getTime()
+      }
+    }
+    xorShiftSeed ^= xorShiftSeed << 13 & 0xFFFFFFFF
+    xorShiftSeed ^= xorShiftSeed >> 17 & 0xFFFFFFFF
+    xorShiftSeed ^= xorShiftSeed << 5 & 0xFFFFFFFF
+    const randomValue = Math.abs(xorShiftSeed / 0x100000000) % 1
+    values.push(randomValue)
+    return randomValue
+  } catch (e) {
+    console.log(e)
+    throw e
+  }
+}
+
+export const resetRandomNumberGenerator = () => {
+  xorShiftSeed = undefined
 }
