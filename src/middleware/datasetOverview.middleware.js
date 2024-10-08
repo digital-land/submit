1b11c3ebecbd458e7ee426d9248c67f5ff0b34df
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-10-08 16:26:28 +0100

    Fix bug with issue count not showing on overview page
    During the migration of middleware, we somehow managed to cause this feature to be lost.
    
    https://github.com/digital-land/submit/issues/500

--- src/middleware/datasetOverview.middleware.js
+++ src/middleware/datasetOverview.middleware.js
@@ -45,7 +45,7 @@ const fetchEntityCount = fetchOne({
 })
 
 export const prepareDatasetOverviewTemplateParams = (req, res, next) => {
-  const { orgInfo, specification, columnSummary, entityCount, sources, dataset } = req
+  const { orgInfo, specification, columnSummary, entityCount, sources, dataset, issues } = req
 
   const mappingFields = columnSummary[0].mapping_field?.split(';') ?? []
   const nonMappingFields = columnSummary[0].non_mapping_field?.split(';') ?? []
@@ -89,6 +89,7 @@ export const prepareDatasetOverviewTemplateParams = (req, res, next) => {
   req.templateParams = {
     organisation: orgInfo,
     dataset,
+    issueCount: issues.length ?? 0,
     stats: {
       numberOfFieldsSupplied: numberOfFieldsSupplied ?? 0,
       numberOfFieldsMatched: numberOfFieldsMatched ?? 0,
