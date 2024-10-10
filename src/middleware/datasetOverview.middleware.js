e803a348ec7d6b9f53b342ddbcec61495c740d60
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-10-10 13:28:29 +0100

    Gracefully handles missing mapping fields
    There are some instances where mapping fields is missing

--- src/middleware/datasetOverview.middleware.js
+++ src/middleware/datasetOverview.middleware.js
@@ -64,8 +64,8 @@ const fetchEntityCount = fetchOne({
 export const prepareDatasetOverviewTemplateParams = (req, res, next) => {
   const { orgInfo, specification, columnSummary, entityCount, sources, dataset, issues } = req
 
-  const mappingFields = columnSummary[0].mapping_field?.split(';') ?? []
-  const nonMappingFields = columnSummary[0].non_mapping_field?.split(';') ?? []
+  const mappingFields = columnSummary[0]?.mapping_field?.split(';') ?? []
+  const nonMappingFields = columnSummary[0]?.non_mapping_field?.split(';') ?? []
   const allFields = [...mappingFields, ...nonMappingFields]
 
   const numberOfFieldsSupplied = specification.fields.map(field => field.field).reduce((acc, current) => {
