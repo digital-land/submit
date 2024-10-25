74fd2c3496aadccd4b1ca25b3fd48be1faea89cf
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-10-26 00:05:11 +0100

    add cookie banner tests

---
+++ test/unit/views/components/cookie-banner.test.js
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest'
+import jsdom from 'jsdom'
+import { setupNunjucks } from '../../../../src/serverSetup/nunjucks.js'
+
+describe('Cookie banner component', () => {
+  const htmlString = '{% include "components/cookie-banner.html" %}'
+  const nunjucks = setupNunjucks({ datasetNameMapping: new Map() })
+  const html = nunjucks.renderString(htmlString, {})
+  const dom = new jsdom.JSDOM(html)
+  const document = dom.window.document
+
+  it('Renders the cookie banner correctly', () => {
+    const header = document.querySelector('.govuk-cookie-banner__heading')
+    const acceptButton = document.querySelector('.js-app-c-cookie-banner__accept')
+    const rejectButton = document.querySelector('.js-app-c-cookie-banner__reject')
+    const viewCookieLink = document.querySelector('.govuk-button-group .govuk-link')
+
+    expect(header.textContent.trim()).toEqual('Cookies on Submit and update your planning data')
+    expect(acceptButton.textContent.trim()).toEqual('Accept analytics cookies')
+    expect(rejectButton.textContent.trim()).toEqual('Reject analytics cookies')
+    expect(viewCookieLink.textContent.trim()).toEqual('View cookies')
+    expect(viewCookieLink.href).toEqual('/cookies')
+  })
+})
