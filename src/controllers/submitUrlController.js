6883f8b870494a1d6016646bbbd03eb54ac1a20a
George Goodall <ggoodall@scottlogic.com>
2024-11-05 16:38:30 +0000

    dont perform local head validation if head request fails or is not allowed

--- src/controllers/submitUrlController.js
+++ src/controllers/submitUrlController.js
@@ -8,6 +8,8 @@ import axios from 'axios'
 import { allowedFileTypes } from '../utils/utils.js'
 import config from '../../config/index.js'
 
+const HTTP_STATUS_METHOD_NOT_ALLOWED = 405
+
 class SubmitUrlController extends UploadController {
   async post (req, res, next) {
     const localValidationErrorType = await SubmitUrlController.localUrlValidation(req.body.url)
@@ -61,7 +63,24 @@ class SubmitUrlController extends UploadController {
       { type: 'filetype', fn: () => SubmitUrlController.validateAcceptedFileType(resp) },
       { type: 'size', fn: () => SubmitUrlController.urlResponseIsNotTooLarge(resp) }
     ])
-    const headResponse = await SubmitUrlController.headRequest(url)
+    let headResponse
+    try {
+      headResponse = await SubmitUrlController.headRequest(url)
+    } catch (error) {
+      logger.warn('submitUrlController/localUrlValidation: failed to get the submitted urls head. skipping post validators', {
+        type: types.DataFetch,
+        errorMessage: error.message
+      })
+      return null
+    }
+
+    if (headResponse.status === HTTP_STATUS_METHOD_NOT_ALLOWED) {
+      // HEAD request not allowed, return null or a specific error message
+      logger.warn('submitUrlController/localUrlValidation: failed to get the submitted urls head as it was not allowed (405) skipping post validators', {
+        type: types.DataFetch
+      })
+      return null
+    }
 
     return postValidators(headResponse).find(validator => !validator.fn())?.type
   }
