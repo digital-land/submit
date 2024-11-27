69d3a83ede352679be9dfc386c06096cfc7764d5
George Goodall <ggoodall@scottlogic.com>
2024-11-27 11:19:37 +0000

    fix issue details pagination link

--- src/middleware/issueDetails.middleware.js
+++ src/middleware/issueDetails.middleware.js
@@ -182,7 +182,7 @@ export function prepareIssueDetailsTemplateParams (req, res, next) {
   let errorHeading
   let issueItems
 
-  const BaseSubpath = `/organisations/${lpa}/${datasetId}/${issueType}/${issueField}/entity/`
+  const BaseSubpath = `/organisations/${lpa}/${datasetId}/${issueType}/${issueField}/entry/`
 
   if (entryNumberCount < entityCount) {
     errorHeading = performanceDbApi.getTaskMessage({ issue_type: issueType, num_issues: issueEntitiesCount, entityCount, field: issueField }, true)
