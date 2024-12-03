b424b4d5184887dfa911494542a60e483d278742
George Goodall <ggoodall@scottlogic.com>
2024-12-03 14:31:09 +0000

    add tests for getIssueDetails

--- test/unit/middleware/entryIssueDetails.middleware.test.js
+++ test/unit/middleware/entryIssueDetails.middleware.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from 'vitest'
-import { addResourceMetaDataToResources, prepareEntry, setRecordCount } from '../../../src/middleware/entryIssueDetails.middleware'
+import { addResourceMetaDataToResources, getIssueDetails, prepareEntry, setRecordCount } from '../../../src/middleware/entryIssueDetails.middleware'
 
 describe('entryIssueDetails.middleware.test.js', () => {
   describe('addResourceMetaDataToResources', () => {
@@ -153,11 +153,74 @@ describe('entryIssueDetails.middleware.test.js', () => {
     })
   })
 
-  describe('prepareEntryIssueDetailsTemplateParams', () => {
-
-  })
-
   describe('getIssueDetails', () => {
+    it('should call render using the provided template params and correct view', () => {
+      const req = {
+        templateParams: {
+          organisation: {
+            name: 'mock lpa',
+            organisation: 'ORG'
+          },
+          dataset: {
+            name: 'mock dataset',
+            dataset: 'mock-dataset',
+            collection: 'mock-collection'
+          },
+          errorSummary: {
+            heading: 'mockMessageFor: 0',
+            items: [
+              {
+                html: 'mock task message 1 in record 1',
+                href: '/organisations/test-lpa/test-dataset/test-issue-type/test-issue-field/1'
+              }
+            ]
+          },
+          entry: {
+            title: 'entry: 1',
+            fields: [
+              {
+                key: { text: 'start-date' },
+                value: { html: '<p class="govuk-error-message">mock message</p>02-02-2022' },
+                classes: 'dl-summary-card-list__row--error'
+              },
+              {
+                classes: '',
+                key: {
+                  text: 'geometry'
+                },
+                value: {
+                  html: 'POINT(0 0)'
+                }
+              }
+            ],
+            geometries: ['POINT(0 0)']
+          },
+          issueType: 'test-issue-type',
+          pagination: {
+            items: [{
+              current: true,
+              href: '/organisations/test-lpa/test-dataset/test-issue-type/test-issue-field/1',
+              number: 1,
+              type: 'number'
+            }]
+          },
+          pageNumber: 1,
+          dataRange: {
+            minRow: 0,
+            maxRow: 50,
+            totalRows: 150
+          }
+        }
+      }
 
+      const res = {
+        render: vi.fn()
+      }
+
+      getIssueDetails(req, res, () => {})
+
+      expect(res.render).toHaveBeenCalledTimes(1)
+      expect(res.render).toHaveBeenCalledWith('organisations/issueDetails.html', req.templateParams)
+    })
   })
 })
