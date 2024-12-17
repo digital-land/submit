0ac44c4a06e3e0d75478ef0a384b580fecde7eab
George Goodall <ggoodall@scottlogic.com>
2024-12-17 11:53:18 +0000

    add additional validation to prepare entity

--- src/middleware/entryIssueDetails.middleware.js
+++ src/middleware/entryIssueDetails.middleware.js
@@ -69,6 +69,12 @@ export const prepareEntry = (req, res, next) => {
 
   const issue = entryIssues[0]
 
+  if (!issue.entry_number || !issue.issue_type || typeof issue.line_number !== 'number') {
+    const error = new Error('Invalid entry issue structure')
+    error.status = 500
+    return next(error)
+  }
+
   const errorMessage = issue.message || issue.issue_type
 
   req.entry = {
