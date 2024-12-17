84e525137d1107827c3d1a0c3cbe12bc205838a7
Roland Sadowski <szabla@gmail.com>
2024-12-17 14:02:43 +0000

    serve the robots.txt file

--- src/serverSetup/middlewares.js
+++ src/serverSetup/middlewares.js
@@ -24,6 +24,7 @@ export function setupMiddlewares (app) {
   app.use('/assets', express.static('./node_modules/govuk-frontend/dist/govuk/assets'))
   app.use('/assets', express.static('./node_modules/@x-govuk/govuk-prototype-components/x-govuk'))
   app.use('/public', express.static('./public'))
+  app.use('/robots.txt', express.static('./robots.txt'))
 
   app.use(cookieParser())
   app.use(bodyParser.urlencoded({ extended: true }))
