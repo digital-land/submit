fb394bee11d393924e7fe6eab549f0cedef4457d
Roland Sadowski <szabla@gmail.com>
2025-01-09 15:40:47 +0000

    ts: PageController fixes

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 import hmpoFormWizard from 'hmpo-form-wizard'
 import { logPageView, types } from '../utils/logging.js'
 import logger from '../utils/logger.js'
@@ -5,9 +6,18 @@ import { datasetSlugToReadableName } from '../utils/datasetSlugToReadableName.js
 const { Controller } = hmpoFormWizard
 
 /**
+ * @typedef {import('express').Request} Request
+ * @typedef {import('express').Response} Response
+ * @typedef {import('express').NextFunction} NextFunction
+ *
+ * @typedef {{ ['data-subject']: string, orgName: string, orgId: any, dataset: string, datasetName: string }} DeepLinkInfo
+ * @typedef {{ options: { deepLink?: DeepLinkInfo, datasetName?: string, backLinkText?: string, lastPage?: string }}} FormOptions
+ */
+
+/**
  * If we arrived at the page via deep from another page, we'll use that page as the back link.
  *
- * @param {string} url current page URL
+ * @param {string} currentUrl current page URL
  * @param {{ referrer?: string, dataset: string }} deepLinkInfo deep link info from the session
  * @returns {string|undefined} back link URL
  */
@@ -28,14 +38,34 @@ function wizardBackLink (currentUrl, deepLinkInfo) {
 class PageController extends Controller {
   checkToolDeepLinkSessionKey = 'check-tool-deep-link'
 
+  /**
+   * @returns {{ route: string, backLink?: string }}
+   */
+  get opts () {
+    // @ts-ignore
+    return this.options
+  }
+
+  /**
+   *
+   * @param {Request & { sessionID: any }} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
   get (req, res, next) {
-    logPageView(this.options.route, req.sessionID, req.ip)
+    logPageView(this.opts.route, req.sessionID)
     super.get(req, res, next)
   }
 
+  /**
+   * @param {Request & { sessionModel: Map<string, any>, form: FormOptions }} req
+   * @param {Response} res
+   * @param {NextFunction} next
+   */
   locals (req, res, next) {
     try {
       let backLink
+      /** @type {DeepLinkInfo} */
       const deepLinkInfo = req?.sessionModel?.get(this.checkToolDeepLinkSessionKey)
       if (deepLinkInfo) {
         req.form.options.deepLink = deepLinkInfo
@@ -47,14 +77,15 @@ class PageController extends Controller {
         req.form.options.backLinkText = `Back to ${deepLinkInfo.datasetName} overview`
       }
 
-      backLink = backLink ?? this.options.backLink
+      backLink = backLink ?? this.opts.backLink
       if (backLink) {
         req.form.options.lastPage = backLink
       }
     } catch (e) {
       logger.warn('PageController.locals(): error setting back link', {
         type: types.App,
-        errorMessage: e.message
+        errorMessage: e instanceof Error ? e.message : 'cought a non error value',
+        errorValue: e
       })
     }
 
