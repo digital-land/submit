d462e63bf26400befd1b2bf38563bc9aa4a926c2
George Goodall <ggoodall@scottlogic.com>
2025-01-22 16:38:22 +0000

    fix tests

--- test/unit/check-answers.test.js
+++ test/unit/check-answers.test.js
@@ -25,9 +25,9 @@ describe('check-answers View', async () => {
   })
 
   it('should render the lpa selected', () => {
-    const lpaRegex = new RegExp('<div class="govuk-summary-list__row">.*Local planning authority.*mockLpa.*Change.*</div>', 'g')
+    const lpaRegex = new RegExp('<div class="govuk-summary-list__row govuk-summary-list__row--no-actions">.*Local planning authority.*mockLpa.*Change.*</div>', 'g')
     expect(html).toMatch(lpaRegex)
-  })
+  }) 
 
   it('should render the name entered', () => {
     const nameRegex = new RegExp('<div class="govuk-summary-list__row">.*Full name.*mockName.*Change.*</div>', 'g')
@@ -35,27 +35,27 @@ describe('check-answers View', async () => {
   })
 
   it('should render the email entered', () => {
-    const emailRegex = new RegExp('<div class="govuk-summary-list__row">.*Email address.*mockEmail.*Change.*</div>', 'g')
+    const emailRegex = new RegExp('<div class="govuk-summary-list__row">.*Email address.*mockEmail.*</div>', 'g')
     expect(html).toMatch(emailRegex)
   })
 
   it('should render the dataset entered', () => {
-    const datasetRegex = new RegExp('<div class="govuk-summary-list__row">.*Dataset.*mockDataset.*Change.*</div>', 'g')
+    const datasetRegex = new RegExp('<div class="govuk-summary-list__row govuk-summary-list__row--no-actions">.*Dataset.*mockDataset.*</div>', 'g')
     expect(html).toMatch(datasetRegex)
   })
 
   it('should render the endpoint url entered', () => {
-    const endpointUrlRegex = new RegExp('<div class="govuk-summary-list__row">.*Dataset URL.*mockEndpointUrl.*Change.*</div>', 'g')
+    const endpointUrlRegex = new RegExp('<div class="govuk-summary-list__row">.*Endpoint URL.*mockEndpointUrl.*Change.*</div>', 'g')
     expect(html).toMatch(endpointUrlRegex)
   })
 
   it('should render the documentation url entered', () => {
-    const documentationUrlRegex = new RegExp('<div class="govuk-summary-list__row">.*Documentation URL.*mockDocumentationUrl.*Change.*</div>', 'g')
+    const documentationUrlRegex = new RegExp('<div class="govuk-summary-list__row">.*Webpage URL.*mockDocumentationUrl.*Change.*</div>', 'g')
     expect(html).toMatch(documentationUrlRegex)
   })
 
   it('should render the licence selected as true if the licence has been confirmed', () => {
-    const hasLicenceRegex = new RegExp('<div class="govuk-summary-list__row">.*Licence.*True.*Change.*</div>', 'g')
+    const hasLicenceRegex = new RegExp('<div class="govuk-summary-list__row">.*Licence.*Yes.*Change.*</div>', 'g')
     expect(html).toMatch(hasLicenceRegex)
   })
 
@@ -68,7 +68,7 @@ describe('check-answers View', async () => {
     }
     const html = stripWhitespace(nunjucks.render('check-answers.html', noLicenseParams))
 
-    const hasLicenceRegex = new RegExp('<div class="govuk-summary-list__row">.*Licence.*False.*Change.*</div>', 'g')
+    const hasLicenceRegex = new RegExp('<div class="govuk-summary-list__row">.*Licence.*No.*Change.*</div>', 'g')
     expect(html).toMatch(hasLicenceRegex)
   })
 })
