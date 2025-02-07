7ff223e2af90deb3e9fd78744746c36a7636b8cc
George Goodall <ggoodall@scottlogic.com>
2025-02-07 11:39:09 +0000

    add debug messages and add in another test

--- test/acceptance/request_check.test.js
+++ test/acceptance/request_check.test.js
@@ -12,71 +12,66 @@ import UploadMethodPage, { uploadMethods } from '../PageObjectModels/uploadMetho
 test.setTimeout(300000)
 
 const navigateToCheck = async (page) => {
+  console.log('navigate to check')
   await page.goto('/check/link?dataset=article-4-direction&orgName=Adur%20District%20Council&orgId=local-authority%3AADU')
+  console.log('navigated to check')
   return new UploadMethodPage(page)
 }
 
 test.describe('Request Check', () => {
   test.describe('with javascript enabled', () => {
-    // test('request check of a @datafile', async ({ page }) => {
-    //   const uploadMethodPage = await navigateToCheck(page)
+    // test/acceptance/request_check.test.js (23-46)
+    test('request check of a @datafile', async ({ page }) => {
+      console.log('Starting test: request check of a @datafile')
 
-    //   await uploadMethodPage.waitForPage()
-    //   await uploadMethodPage.selectUploadMethod(uploadMethods.File)
-    //   const uploadFilePage = await uploadMethodPage.clickContinue()
+      const uploadMethodPage = await navigateToCheck(page)
+      console.log('Successfully navigated to the upload method page')
 
-    //   await uploadFilePage.waitForPage()
-    //   await uploadFilePage.uploadFile('test/datafiles/article4directionareas-ok.csv')
-    //   const statusPage = await uploadFilePage.clickContinue()
+      await uploadMethodPage.waitForPage()
+      console.log('Upload method page loaded')
+      await uploadMethodPage.selectUploadMethod(uploadMethods.File)
+      console.log('Selected file upload method')
+      const uploadFilePage = await uploadMethodPage.clickContinue()
 
-    //   await statusPage.waitForPage()
-    //   await statusPage.expectPageToBeProcessing()
-    //   await statusPage.expectPageToHaveFinishedProcessing()
-    //   const id = await statusPage.getIdFromUrl()
-    //   const resultsPage = await statusPage.clickContinue()
+      await uploadFilePage.waitForPage()
+      console.log('Upload file page loaded')
+      await uploadFilePage.uploadFile('test/datafiles/article4directionareas-ok.csv')
+      console.log('File uploaded')
+      const statusPage = await uploadFilePage.clickContinue()
 
-    //   await resultsPage.waitForPage(id)
-    //   await resultsPage.expectPageHasTitle()
-    //   await resultsPage.expectPageHasTabs()
+      await statusPage.waitForPage()
+      console.log('Status page loaded')
+      await statusPage.expectPageToBeProcessing()
+      console.log('Page is processing')
+      await statusPage.expectPageToHaveFinishedProcessing()
+      console.log('Page has finished processing')
+      const id = await statusPage.getIdFromUrl()
+      console.log(`Extracted ID from URL: ${id}`)
+      const resultsPage = await statusPage.clickContinue()
 
-    //   const confirmationPage = await resultsPage.clickContinue()
-    //   await confirmationPage.waitForPage()
-    // })
+      await resultsPage.waitForPage(id)
+      console.log('Results page loaded')
+      await resultsPage.expectPageHasTitle()
+      console.log('Results page has title')
+      await resultsPage.expectPageHasTabs()
+      console.log('Results page has tabs')
 
-    // test('request check of an error @datafile', async ({ page }) => {
-    //   const uploadMethodPage = await navigateToCheck(page)
+      const confirmationPage = await resultsPage.clickContinue()
+      console.log('Navigated to confirmation page')
+      await confirmationPage.waitForPage()
+      console.log('Confirmation page loaded')
+    })
 
-    //   await uploadMethodPage.waitForPage()
-    //   await uploadMethodPage.selectUploadMethod(uploadMethods.File)
-    //   const uploadFilePage = await uploadMethodPage.clickContinue()
-
-    //   await uploadFilePage.waitForPage()
-    //   await uploadFilePage.uploadFile('test/datafiles/article4directionareas-error.csv')
-    //   const statusPage = await uploadFilePage.clickContinue()
-
-    //   await statusPage.waitForPage()
-    //   await statusPage.expectPageToBeProcessing()
-    //   await statusPage.expectPageToHaveFinishedProcessing()
-    //   const id = await statusPage.getIdFromUrl()
-    //   const resultsPage = await statusPage.clickContinue()
-
-    //   await resultsPage.waitForPage(id)
-    //   await resultsPage.expectPageHasTitle()
-
-    //   await resultsPage.expectPageToHaveBlockingErrors()
-    //   await resultsPage.expectPageHasTabs()
-    // })
-
-    test('request check of a @url', async ({ page }) => {
+    test('request check of an error @datafile', async ({ page }) => {
       const uploadMethodPage = await navigateToCheck(page)
 
       await uploadMethodPage.waitForPage()
-      await uploadMethodPage.selectUploadMethod(uploadMethods.URL)
-      const submitURLPage = await uploadMethodPage.clickContinue()
+      await uploadMethodPage.selectUploadMethod(uploadMethods.File)
+      const uploadFilePage = await uploadMethodPage.clickContinue()
 
-      await submitURLPage.waitForPage()
-      await submitURLPage.enterURL('https://raw.githubusercontent.com/digital-land/PublishExamples/refs/heads/main/Article4Direction/Files/Article4DirectionArea/article4directionareas-(Permitted-development-rights%20column%20missing).csv')
-      const statusPage = await submitURLPage.clickContinue()
+      await uploadFilePage.waitForPage()
+      await uploadFilePage.uploadFile('test/datafiles/article4directionareas-error.csv')
+      const statusPage = await uploadFilePage.clickContinue()
 
       await statusPage.waitForPage()
       await statusPage.expectPageToBeProcessing()
@@ -86,7 +81,50 @@ test.describe('Request Check', () => {
 
       await resultsPage.waitForPage(id)
       await resultsPage.expectPageHasTitle()
+
+      await resultsPage.expectPageToHaveBlockingErrors()
       await resultsPage.expectPageHasTabs()
+    })
+
+    // test/acceptance/request_check.test.js (72-92)
+    test('request check of a @url', async ({ page }) => {
+      console.log('Starting test: request check of a @url')
+
+      const uploadMethodPage = await navigateToCheck(page)
+      console.log('Navigated to check page')
+
+      await uploadMethodPage.waitForPage()
+      console.log('Upload method page loaded')
+      await uploadMethodPage.selectUploadMethod(uploadMethods.URL)
+      console.log('Selected upload method: URL')
+      const submitURLPage = await uploadMethodPage.clickContinue()
+      console.log('Clicked continue to submit URL page')
+
+      await submitURLPage.waitForPage()
+      console.log('Submit URL page loaded')
+      await submitURLPage.enterURL('https://raw.githubusercontent.com/digital-land/PublishExamples/refs/heads/main/Article4Direction/Files/Article4DirectionArea/article4directionareas-(Permitted-development-rights%20column%20missing).csv')
+      console.log('Entered URL')
+      const statusPage = await submitURLPage.clickContinue()
+      console.log('Clicked continue to status page')
+
+      await statusPage.waitForPage()
+      console.log('Status page loaded')
+      await statusPage.expectPageToBeProcessing()
+      console.log('Page is processing')
+      await statusPage.expectPageToHaveFinishedProcessing()
+      console.log('Page finished processing')
+      const id = await statusPage.getIdFromUrl()
+      console.log(`Got ID from URL: ${id}`)
+      const resultsPage = await statusPage.clickContinue()
+      console.log('Clicked continue to results page')
+
+      await resultsPage.waitForPage(id)
+      console.log('Results page loaded')
+      await resultsPage.expectPageHasTitle()
+      console.log('Page has title')
+      await resultsPage.expectPageHasTabs()
+      console.log('Page has tabs')
+      console.log('Test completed')
     })
 
     // test('request check of an error @url', async ({ page }) => {
