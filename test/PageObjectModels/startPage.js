f6e5132a0d5da6165f375dae90b6ec98e0812323
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2025-03-20 17:35:35 +0000

    Redirects `/check` and `/submit` to landing page
    This is to ensure users always go through our main user journey with proper deep linking set up.

--- test/PageObjectModels/startPage.js
+++ test/PageObjectModels/startPage.js
@@ -3,7 +3,7 @@ import DatasetPage from './datasetPage'
 
 export default class StartPage extends BasePage {
   constructor (page) {
-    super(page, '/check/')
+    super(page, '/check/link?dataset=brownfield-land&orgName=Adur%20District%20Council&orgId=local-authority%3AADU')
   }
 
   async clickStartNow (skipVerification) {
