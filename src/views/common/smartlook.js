af7623f7139c773c6acc927d3c1c6ea3925d72ff
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2025-04-07 15:32:44 +0100

    Enable forms to be recorded in smartlook
    https://github.com/digital-land/submit/issues/362
    
    By default, smartlook hide values entered in text inputs, however, our platform is public and doesn't require this.

--- src/views/common/smartlook.js
+++ src/views/common/smartlook.js
@@ -3,4 +3,8 @@ window.smartlook || (function (d) {
   const c = d.createElement('script'); o.api = new Array(); c.async = true; c.type = 'text/javascript'
   c.charset = 'utf-8'; c.src = 'https://web-sdk.smartlook.com/recorder.js'; h.appendChild(c)
 })(document)
-smartlook('init', '{{ smartlookKey }}', { region: '{{ smartlookRegion }}' })
+
+smartlook("init", "{{ smartlookKey }}", {
+  region: "{{ smartlookRegion }}",
+  forms: true,
+});
