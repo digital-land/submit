8b6b4cf8e4c65b6e96a29527df29bd5de464a83d
Feba <feba.rajan@levellingup.gov.uk>
2025-06-26 15:31:29 +0100

    update endpoint status check to only show error if all endpoints fail

--- src/middleware/lpa-overview.middleware.js
+++ src/middleware/lpa-overview.middleware.js
@@ -205,7 +205,6 @@ export const addNoticesToDatasets = (req, res, next) => {
  */
 export function prepareDatasetObjects (req, res, next) {
   const { issues, endpoints, expectationOutOfBounds, availableDatasets } = req
-
   const outOfBoundsViolations = new Set((expectationOutOfBounds ?? []).map(o => o.dataset))
   req.datasets = availableDatasets.map((dataset) => {
     const datasetEndpoints = endpoints[dataset]
@@ -216,7 +215,10 @@ export function prepareDatasetObjects (req, res, next) {
     }
 
     const endpointCount = datasetEndpoints.length
-    const httpStatus = datasetEndpoints.find(endpoint => endpoint.latest_status !== '200')?.latest_status
+    // const httpStatus = datasetEndpoints.find(endpoint => endpoint.latest_status !== '200')?.latest_status
+    // const error = httpStatus !== undefined ? `There was a ${httpStatus} error accessing the endpoint URL` : undefined
+    const allError = datasetEndpoints.every(endpoint => endpoint.latest_status !== '200')
+    const httpStatus = allError ? datasetEndpoints[0]?.latest_status : undefined
     const error = httpStatus !== undefined ? `There was a ${httpStatus} error accessing the endpoint URL` : undefined
     const expectationFailed = outOfBoundsViolations.has(dataset)
     const issueCount = (datasetIssues?.length || 0) + (expectationFailed ? 1 : 0)
@@ -351,7 +353,6 @@ const fetchOutOfBoundsExpectations = expectationFetcher({
   expectation: expectations.entitiesOutOfBounds,
   result: 'expectationOutOfBounds'
 })
-
 /**
  * Organisation (LPA) overview page middleware chain.
  */
