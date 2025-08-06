fd52bfad5599cf588922b4f50c6a68e8a23eb0c8
Feba <feba.rajan@levellingup.gov.uk>
2025-08-06 11:53:47 +0100

    coderabbitai refactor suggestion

--- src/utils/datasetLoader.js
+++ src/utils/datasetLoader.js
@@ -1,12 +1,18 @@
-// src/utils/datasetsLoader.js
+// src/utils/datasetLoader.js
 import config from '../../config/index.js'
 
 export async function getDatasetNameMap () {
   const res = await fetch(`${config.mainWebsiteUrl}/dataset.json`)
   if (!res.ok) throw new Error(`Failed to fetch datasets from API: ${res.statusText}`)
 
-  const { datasets } = await res.json()
+  const data = await res.json()
+  const { datasets } = data || {}
+  if (!Array.isArray(datasets)) {
+    throw new Error('Invalid API response: datasets is not an array')
+  }
   return Object.fromEntries(
-    datasets.map(d => [d.dataset, d.name])
+    datasets
+      .filter(d => d && d.dataset && d.name)
+      .map(d => [d.dataset, d.name])
   )
 }
