3caac3b7083003f19b8d8dc3a06019ca5bc9f8f7
Matt Poole <matt.a.poole@outlook.com>
2025-11-17 12:11:38 +0000

    added a new service that allows entity queires to platfrom REST API, and moved dataset table to use this

--- src/middleware/dataview.middleware.js
+++ src/middleware/dataview.middleware.js
@@ -28,14 +28,33 @@ export const dataviewQueryParams = v.object({
   resourceId: v.optional(v.string())
 })
 
+
+
 const validatedataviewQueryParams = validateQueryParams({
   schema: dataviewQueryParams
+})
+
+/**
+ * Middleware. Updates req with 'entities' same as fetchEntities so not to be used together!
+ *
+ * Fetches entities from the Platform API (mainWebsiteUrl) instead of Datasette.
+ * Uses REST API with query parameters instead of SQL.
+ */
+export const fetchEntitiesPlatformDb = fetchMany({
+  query: ({ req, params }) => ({
+    organisation_entity: req.orgInfo.entity,
+    dataset: params.dataset,
+    limit: req.dataRange.pageLength,
+    offset: req.dataRange.offset
+  }),
+  result: 'entities',
+  dataset: FetchOptions.platformDb
 })
 
 export const fetchEntities = fetchMany({
   query: ({ req, params }) => `SELECT * FROM entity WHERE organisation_entity = ${req.orgInfo.entity} LIMIT ${req.dataRange.pageLength} OFFSET ${req.dataRange.offset}`,
   dataset: FetchOptions.fromParams,
-  result: 'entities'
+  result: 'entities' 
 })
 
 export const setRecordCount = (req, res, next) => {
@@ -116,7 +135,7 @@ export default [
   getSetDataRange(config.tablePageLength),
   show404IfPageNumberNotInRange,
 
-  fetchEntities,
+  fetchEntitiesPlatformDb,
   extractJsonFieldFromEntities,
   replaceUnderscoreInEntities,
 
---
+++ src/services/platformApi.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import logger from '../utils/logger.js'
+import { types } from '../utils/logging.js'
+import config from '../../config/index.js'
+
+/**
+ * Service for querying the Platform API (mainWebsiteUrl)
+ * Unlike Datasette which uses SQL queries, this uses REST endpoints with query parameters
+ */
+export default {
+  /**
+   * Fetches entities from the Platform API
+   *
+   * @param {Object} params - Query parameters
+   * @param {string} params.organisation_entity - The organisation entity ID
+   * @param {string} params.dataset - The dataset name
+   * @param {number} [params.limit] - Maximum number of results
+   * @param {number} [params.offset] - Number of results to skip
+   * @returns {Promise<{data: object, formattedData: object[]}>} - A promise that resolves to formatted entity data
+   * @throws {Error} If the query fails or there is an error communicating with the Platform API
+   */
+  fetchEntities: async (params) => {
+    const queryParams = new URLSearchParams()
+    
+    if (params.organisation_entity) queryParams.append('organisation_entity', params.organisation_entity)
+    if (params.dataset) queryParams.append('dataset', params.dataset)
+    if (params.limit) queryParams.append('limit', params.limit)
+    if (params.offset) queryParams.append('offset', params.offset)
+
+    const url = `${config.mainWebsiteUrl}/entity.json?${queryParams.toString()}`
+
+    try {
+      logger.debug({ message: 'Platform API request', type: types.DataFetch, url, params })
+      const response = await axios.get(url)
+      
+      // Platform API returns { entities: [...] }
+      const entities = response.data?.entities || []
+      
+      return {
+        data: response.data,
+        formattedData: entities
+      }
+    } catch (error) {
+      logger.warn({ 
+        message: `platformApi.fetchEntities(): ${error.message}`, 
+        type: types.App, 
+        params, 
+        platformUrl: config.mainWebsiteUrl 
+      })
+      throw error
+    }
+  }
+}
