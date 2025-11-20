3a40348b0ebbf39efd060f622f18fef2778ce40b
Matt Poole <matt.a.poole@outlook.com>
2025-11-20 13:44:15 +0000

    adding encouraged datasets to same group as prospective the can-provide segment

--- src/middleware/lpa-overview.middleware.js
+++ src/middleware/lpa-overview.middleware.js
@@ -287,6 +287,8 @@ export function prepareOverviewTemplateParams (req, res, next) {
         return 'expected'
       case 'prospective':
         return 'prospective'
+      case 'encouraged': //Currently adding encouraged datasets to same group as prospective the "can-provide" segment
+        return 'prospective'
       default:
         return 'other'
     }
