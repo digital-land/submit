3bb54a3c809a3640178c9296235dc31f7912ebb4
Matt Poole <matt.a.poole@outlook.com>
2025-12-02 11:50:25 +0000

    test staging with parallel org info

--- src/middleware/lpa-overview.middleware.js
+++ src/middleware/lpa-overview.middleware.js
@@ -352,8 +352,8 @@ const fetchOutOfBoundsExpectations = expectationFetcher({
  * Organisation (LPA) overview page middleware chain.
  */
 export default [
-  fetchOrgInfo,
   parallel([
+    fetchOrgInfo,
     fetchEndpointSummary,
     fetchEntityIssueCountsPerformanceDb,
     fetchProvisions
