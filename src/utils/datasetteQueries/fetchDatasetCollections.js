63bec85d37717238eee6f9c8811a98f74ce7a9e8
Matt Poole <matt.a.poole@outlook.com>
2026-04-10 15:46:39 +0100

    disable no database filter out of datasets, allows supplementary plan, add alternate pre populated notice for local plan

--- src/utils/datasetteQueries/fetchDatasetCollections.js
+++ src/utils/datasetteQueries/fetchDatasetCollections.js
@@ -1,6 +1,5 @@
 import datasette from '../../services/datasette.js'
 import logger from '../logger.js'
-import { types } from '../logging.js'
 
 // Temporary utility to get dataset collection slug-name mapping from Datasette, likely/hopefully don't need collection names in the future.
 export const getDatasetCollectionSlugNameMapping = async (nameMap) => {
@@ -10,26 +9,8 @@ export const getDatasetCollectionSlugNameMapping = async (nameMap) => {
       `SELECT dataset, collection FROM dataset WHERE dataset IN (${datasets})`
     )
 
-    // If no dataset table exists for a dataset, remove from datasetSlugNameMapping
-    const validDatasets = []
-
-    for (const row of datasetSlugNameTable.formattedData) {
-      try {
-        // Test if the dataset table exists by running a simple query, for example datasets such as development-plan-geography exist in dataset, but no table created yet in datasette
-        await datasette.runQuery('SELECT 1 FROM entity LIMIT 1', row.dataset)
-        validDatasets.push(row)
-      } catch (error) {
-        logger.info(`Dataset table '${row.dataset}' does not exist, removing from mapping`, {
-          dataset: row.dataset,
-          errorMessage: error.message,
-          type: types.DataFetch
-        })
-        // Don't add to validDatasets
-      }
-    }
-
     const datasetMapping = new Map()
-    validDatasets.forEach(row => {
+    datasetSlugNameTable.formattedData.forEach(row => {
       datasetMapping.set(row.dataset, row.collection)
     })
     return datasetMapping
