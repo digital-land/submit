a468a33b4d1deb2a40a5cc93f5aba57b31f30cab
Gibah Joseph <gibahjoe@gmail.com>
2026-09-08 14:32:54 +0100

    test: Update main layout tests to check for third-party analytics scripts based on cookie acceptance

--- test/unit/views/layouts/main.test.js
+++ test/unit/views/layouts/main.test.js
@@ -4,8 +4,8 @@ import { setupNunjucks } from '../../../../src/serverSetup/nunjucks.js'
 const nunjucks = setupNunjucks({ datasetNameMapping: new Map() })
 
 describe('Main layout tests', () => {
-  it('does not include third-party analytics scripts', () => {
-    const html = nunjucks.render('layouts/main.html')
+  it.each([true, false])('does not include third-party analytics scripts when cookiesAccepted is %s', (cookiesAccepted) => {
+    const html = nunjucks.render('layouts/main.html', { cookiesAccepted })
 
     expect(html).not.toContain('googletagmanager.com')
     expect(html).not.toContain('smartlook.com')
